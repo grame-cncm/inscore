dea7c3b906e5fefb702433b2f10ea3913bd97236
Dominique Fober <fober@grame.fr>
2011-12-06 10:14:36 +0100

    dummy object view added: a view that do nothing

--- src/view/VDummyObjectView.h
+++ src/view/VDummyObjectView.h
@@ -49,6 +49,7 @@ class VDummyObjectView : public VObjectView
 		virtual void updateView(IObject * object)			{}
 		virtual void updateObjectSize( IObject * object )	{}
 		virtual void setParentView (IObject * object)		{}
+		virtual void setParentItem( VObjectView* master )	{}
 
 		virtual void setEffect (GraphicEffect& effect)		{}
 		virtual GraphicEffect getEffect () const			{ return GraphicEffect(); }
