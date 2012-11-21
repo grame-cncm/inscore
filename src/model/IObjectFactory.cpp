8fd1b5515dcaaae7e82839d3e7ec9cb5a85ee5d9
Dominique Fober <fober@grame.fr>
2012-11-21 10:39:09 +0100

    unused IWatcher removed
    Former-commit-id: 2db6496a2e5f9b210054d2eed8e9c11b7c445674

--- src/model/IObjectFactory.cpp
+++ src/model/IObjectFactory.cpp
@@ -56,11 +56,6 @@ template<typename T> SIObject _create(const std::string& name , IObject* parent)
 	return obj->getView() ? obj : 0;
 }
 
-template<> SIObject _create<IWatcher>(const std::string& name , IObject* parent) 
-{
-	return IWatcher::create(name, parent);
-}
-
 template<> SIObject _create<ISignal>(const std::string& name , IObject* parent) 
 {
 	return ISignal::create(name, parent);
@@ -145,9 +140,6 @@ SIObject IObjectFactory::create(const std::string& name , const std::string& typ
 
 	else if ( type == IVideo::kVideoType )
 		obj = _create<IVideo> (name, parent);
-		
-	else if ( type == IWatcher::kWatcherType )
-		obj = _create<IWatcher> (name, parent);
 
 	else if ( type == ISVGFile::kSVGFileType )
 		obj = _create<ISVGFile> (name, parent);
