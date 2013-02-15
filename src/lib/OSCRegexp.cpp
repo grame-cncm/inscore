722ff05b4f0fd1313b98e22d87a4d99bc47ade07
D. Fober <fober@grame.fr>
2013-02-15 17:26:37 +0100

    return value : cast to bool
    Former-commit-id: 218372c52ae660ab9a7ba335d34fa21b7e3d37a7

--- src/lib/OSCRegexp.cpp
+++ src/lib/OSCRegexp.cpp
@@ -51,7 +51,7 @@ std::string OSCRegexp::OSCRe2Re (const char* re)
 //--------------------------------------------------------------------------
 bool OSCRegexp::match (const char* str) const
 {
-	return fRegexp.MatchExact(str);
+	return bool(fRegexp.MatchExact(str));
 }
 
 }
