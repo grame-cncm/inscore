723689827a35fc76e832760bbf8bca9de7f96ae4
D. Fober <fober@grame.fr>
2013-03-22 15:06:44 +0100

    new javascript node
    Former-commit-id: e8f673ecdab570d5f60d30022f7d974b0adad41a

---
+++ src/scripting/IJavascript.h
@@ -0,0 +1,77 @@
+/*
+
+  INScore Project
+
+  Copyright (C) 2013  Grame
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+  Grame Research Laboratory, 11 cours de Verdun Gensoul, 69002 Lyon - France
+  research@grame.fr
+
+*/
+
+
+#ifndef __IJavascript__
+#define __IJavascript__
+
+
+#include "IVNode.h"
+#include "TScripting.h"
+
+namespace inscore
+{
+
+class IJavascript;
+typedef class libmapping::SMARTP<IJavascript>	SIJavascript;
+
+/*!
+\addtogroup ITLModel
+@{
+*/
+
+//--------------------------------------------------------------------------
+/**
+*	\brief a javascript engine.
+*
+*/
+class IJavascript: public IVNode
+{
+	protected:
+		TScripting	fScripter;
+		
+				 IJavascript(IObject * parent, TJSEngine* js);
+		virtual ~IJavascript() {}
+
+		/// \brief javascript engine \c 'get' message handler
+//		virtual SIMessageList getMsgs (const IMessage* msg) const;
+
+		/// \brief the \c 'run' message handler
+		virtual MsgHandler::msgStatus runMsg(const IMessage* msg);
+
+		// overrides get handlers for color, position and time
+		virtual void colorAble ()		{}
+		virtual void positionAble ()	{}
+		virtual void timeAble ()		{}
+
+	public:
+		static SIJavascript create(IObject * parent, TJSEngine* js) { return new IJavascript(parent, js); }
+};
+
+/*!@} */
+
+} // end namespoace
+
+#endif
