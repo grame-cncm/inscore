85e55d0e33db1cb4328dc707bd3db84a7d958c74
D. Fober <fober@grame.fr>
2013-10-24 10:00:21 +0200

    validation tools revised
    Former-commit-id: d9fa062a6355a0bac0d673bc1524c1c224a939be

--- validation/tools/INScoreVersion.cpp
+++ validation/tools/INScoreVersion.cpp
@@ -1,6 +1,7 @@
 
 
 #include <iostream>
+#include <string>
 
 #include "INScore.h"
 
@@ -8,8 +9,13 @@ using namespace std;
 using namespace inscore;
 
 
+static const string verbopt1 = "-v";
+static const string verbopt2 = "--v";
+
 int main (int argc, char * argv[])
 {
-	cout << "INScore version " << INScore::versionStr() << endl;
+	bool verbose = (argc == 2) && (verbopt1 == argv[1] || verbopt2 == argv[1] );
+	if (verbose) cout << "INScore version ";
+	cout << INScore::versionStr() << endl;
 	return 0;
 }
