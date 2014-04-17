ed673cfc47c7f2470e82324b21ca447c53faaf14
D. Fober <fober@grame.fr>
2014-04-17 11:39:06 +0200

    version number moved to 1.07
    Former-commit-id: 990be1e7acd818ff203689707d0dbbb8b75dd1f2

--- src/interface/INScore.cpp
+++ src/interface/INScore.cpp
@@ -130,8 +130,8 @@ void INScore::stop(IGlue* glue)
 //--------------------------------------------------------------------------
 // versions 
 //--------------------------------------------------------------------------
-float INScore::version	()				{ return 1.06; }
-const char* INScore::versionStr ()		{ return "1.06"; }
+float INScore::version	()				{ return 1.07; }
+const char* INScore::versionStr ()		{ return "1.07"; }
 
 //--------------------------------------------------------------------------
 const char* INScore::guidoversion()
