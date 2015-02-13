f9f7597cc7db6ee0c9c880307241e612f51962a4
guillaume <guillaume.gouilloux@gmail.com>
2015-02-13 14:38:43 +0100

    Add http server library
    Former-commit-id: cf9c826dea9b411f120a6d86fb758d52a35c61e0

---
+++ lib/httpserver/INScoreServer.cpp
@@ -0,0 +1,61 @@
+/*
+  Copyright (c) Grame 2015
+
+  This library is free software; you can redistribute it and modify it under
+  the terms of the GNU Library General Public License as published by the
+  Free Software Foundation version 2 of the License, or any later version.
+
+  This library is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License
+  for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+
+  Grame Research Laboratory, 11, cours Verdun Gensoul 69002 Lyon - France
+  research@grame.fr
+
+*/
+
+#include "HTTPServer.h"
+#include "INScoreServer.h"
+
+using namespace inscorehttpd;
+
+void * initialize()
+{
+	return new HTTPDServer;
+}
+
+void destroy(void * server)
+{
+	HTTPDServer *aServer  = static_cast<HTTPDServer *>(server);
+	delete aServer;
+}
+
+bool start(void * server, int port)
+{
+	HTTPDServer *aServer  = static_cast<HTTPDServer *>(server);
+	if(aServer) {
+		return aServer->start(port);
+	}
+	return false;
+}
+
+bool stop(void * server)
+{
+	HTTPDServer *aServer  = static_cast<HTTPDServer *>(server);
+	if(aServer) {
+		aServer->stop();
+		return true;
+	}
+	return false;
+}
+
+int status(void * server)
+{
+	// TODO
+	return 200;
+}
---
+++ lib/httpserver/Response.cpp
@@ -0,0 +1,77 @@
+/*
+  Copyright (c) Grame 2015
+
+  This library is free software; you can redistribute it and modify it under
+  the terms of the GNU Library General Public License as published by the
+  Free Software Foundation version 2 of the License, or any later version.
+
+  This library is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License
+  for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+
+  Grame Research Laboratory, 11, cours Verdun Gensoul 69002 Lyon - France
+  research@grame.fr
+
+*/
+
+#include "Response.h"
+#include <cstring>
+
+using namespace std;
+
+namespace inscorehttpd {
+
+Response::Response (const char* data, unsigned int size, string format, int http_status)
+{
+	fData = new char[size + 1];
+	memcpy (fData, data, size);
+	fSize = size;
+	fFormat = format;
+	fHttpStatus = http_status;
+}
+
+Response::Response (string data, string format, int http_status)
+{
+	int size = data.size();
+	const char *cc = data.c_str();
+	fData = new char[size + 1];
+	memcpy (fData, cc, size + 1);
+	fSize = size;
+	fFormat = format;
+	fHttpStatus = http_status;
+}
+
+Response::Response ()
+{
+	fData = new char[1];
+	strcpy(fData, "");
+	fSize = 0;
+	fFormat = "text/plain";
+	fHttpStatus = 404;
+}
+
+Response::Response (const Response &copy)
+{
+	fSize = copy.fSize;
+	fData = new char[fSize + 1];
+	memcpy (fData, copy.fData, fSize + 1);
+	fFormat = copy.fFormat;
+	fHttpStatus = copy.fHttpStatus;
+}
+
+Response::~Response ()
+{
+  delete[] fData;
+}
+
+Response Response::genericFailure(string errorString, int http_status)
+{
+	return Response (errorString, "text/plain", http_status);
+}
+
+}
