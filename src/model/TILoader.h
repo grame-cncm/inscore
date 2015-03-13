639c04f02e58ab5c843f45bbe12bc8681e9e6192
D. Fober <fober@grame.fr>
2015-03-13 14:59:14 +0100

    seaparate utility to load inscore files (designed for url support)
    Former-commit-id: cbe1cd75909ffd71c7e2d9ee01bd6f63155436ef

---
+++ src/model/TILoader.h
@@ -0,0 +1,66 @@
+/*
+
+  INScore Project
+
+  Copyright (C) 2015  Grame
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+  Grame Research Laboratory, 9 rue du Garet, 69001 Lyon - France
+  research@grame.fr
+
+*/
+
+
+#ifndef __TILoader__
+#define __TILoader__
+
+#include <istream>
+#include <string>
+
+#include "IMessageHandlers.h"
+#include "TScripting.h"
+
+namespace inscore
+{
+
+class IMessage;
+class IObject;
+//class TJSEngine;
+//class TLua;
+
+//--------------------------------------------------------------------------
+/*!
+	\brief a base class for IAppl and IScene to provide inscore files loading service
+	
+*/
+class TIloader
+{
+	protected:
+				 TIloader() {}
+		virtual ~TIloader() {}
+
+		virtual MsgHandler::msgStatus	load(const IMessage* msg, IObject* client, const std::string& rootpath);
+		virtual void					process (const SIMessageList& msgs, IObject* root, const std::string& baseaddress);
+
+		virtual TJSEngine*		getJSEngine()	= 0;
+		virtual TLua*			getLUAEngine()	= 0;
+
+		static std::string		makeAbsolutePath( const std::string& path, const std::string& file );
+};
+
+} // end namespoace
+
+#endif
