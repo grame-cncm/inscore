32bebdc21d76f6a4646b36fd6c878f1a557fe3a8
guillaume <guillaume.gouilloux@gmail.com>
2015-06-25 19:00:45 +0200

    Add read file in web api
    Former-commit-id: e432b47c2efdc63336965e52204a16e832375477

--- src/library/web/WebApi.cpp
+++ src/library/web/WebApi.cpp
@@ -26,7 +26,7 @@
 #include "VObjectView.h"
 #include "VSceneView.h"
 #include "ITLparser.h"
-#include "abstractdata.h"
+#include "IAppl.h"
 
 #include <QByteArray>
 #include <QWaitCondition>
@@ -34,6 +34,7 @@
 #include <QEvent>
 
 #include <sstream>
+#include <fstream>
 #include <cstring>
 
 using namespace std;
@@ -45,6 +46,7 @@ namespace inscore
     const char * WebApi::kPostCmdMsg = "post";
     const char * WebApi::kClickMsg = "click";
 	const char * WebApi::kHoverMsg = "hover";
+	const char * WebApi::kFileMsg = "file";
 
 	extern QWaitCondition gModelUpdateWaitCondition;	///< a wait condition to wait for model update.
     QMutex WebApi::fPostCommandMutex;
@@ -157,6 +159,71 @@ void WebApi::sendEvent(QGraphicsItem * item, int eventType)
 
         QGraphicsScene * scene = sceneView->scene();
         scene->sendEvent(item, &event);
+}
+
+//--------------------------------------------------------------------------
+AbstractData WebApi::readFile(const std::string& relativePath, bool base64)
+{
+	string path = getAbsoluteFilePath(relativePath);
+	AbstractData data;
+	data.data = 0;
+	data.size = 0;
+
+	if(!path.empty()) {
+		ifstream file;
+		file.open (path.c_str(), ifstream::binary);
+		if (file.is_open()) {
+			// get length of file:
+			file.seekg (0, file.end);
+			data.size = file.tellg();
+			file.seekg (0, file.beg);
+
+			if (data.size) {
+				// allocate memory:
+				data.data = new char [data.size];
+				// read data as a block:
+				file.read (data.data, data.size);
+
+				if(base64) {
+					QByteArray bArray = QByteArray::fromRawData(data.data, data.size);
+					bArray = bArray.toBase64();
+					delete data.data;
+					data.size = bArray.size() + 1;
+					data.data = new char[data.size];
+					strcpy(data.data, bArray.data());
+				}
+			}
+			file.close();
+		}
+	}
+	return data;
+}
+
+//-------------------------------------------------------------------------------.
+string WebApi::getAbsoluteFilePath(const std::string &relativePath)
+{
+	if(relativePath.find("../") != string::npos)
+		return "";
+	return IAppl::absolutePath(relativePath);
+}
+
+//-------------------------------------------------------------------------------.
+const char * WebApi::getMimetype(const string &filepath)
+{
+	string::size_type pos = filepath.rfind(".");
+	string extension;
+	if(pos != string::npos) {
+		extension = filepath.substr(pos + 1);
+	}
+	if(extension == "inscore" || extension == "txt")
+		return "text/plain";
+	else if(extension == "pdf")
+		return "application/pdf";
+	else if(extension == "png")
+		return "image/png";
+	else if(extension == "jpeg" || extension == "jpg")
+		return "image/jpeg";
+	return "application/octet-stream";
 }
 
 }
