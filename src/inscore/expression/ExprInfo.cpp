5ab96006c12af9d542be5ef287e25b59244cf42a
Gabriel LEPETIT-AIMON <gabriel.lepetitaimon@gmail.com>
2016-01-11 16:54:03 +0100

    Add a search and replace tool for Score expression.
    Former-commit-id: 3da5dc902ef6a91b8ce0044f65130eca0b060c02

--- src/inscore/expression/ExprInfo.cpp
+++ src/inscore/expression/ExprInfo.cpp
@@ -5,6 +5,10 @@ namespace inscore{
 
 std::set<std::string> ExprInfo::fileDependency(const SIExpression &expr){return ExprFileDependency::fileDependency(expr);}
 
+void ExprInfo::fileReplace(SIExpression &expr, const filepath &search, const filepath &replace){
+	ExprSearchReplace<filepath>::searchReplace(expr,search,replace);
+}
+
 /****************************************************
  *               ExprFileDependency                 *
  * *************************************************/
@@ -26,5 +30,29 @@ const std::string ExprFileDependency::eval(const IExprOperator *arg, const IExpr
 	((const IExprArgBase*)arg->constArg2())->accept(this);
     return "";
 }
+
+
+
+/****************************************************
+ *                  SearchReplace                   *
+ * *************************************************/
+template <typename T>
+void ExprSearchReplace<T>::searchReplace(SIExpression &expression, T search, T replace)
+{
+	ExprSearchReplace e(search, replace);
+	expression->rootNode()->accept(&e);
+}
+
+template <typename T>
+const std::string ExprSearchReplace<T>::eval(const T &value, IExprArgBase *exprArg){
+	if(value==fSearch){
+		IExprArg<T>* arg = static_cast<IExprArg<T>*>(exprArg);
+		arg->setArg(fReplace);
+	}
+	return "";
+}
+
+
+
 
 } // End namespace
--- src/inscore/expression/ExprInfo.h
+++ src/inscore/expression/ExprInfo.h
@@ -11,13 +11,14 @@ class ExprInfo
 {
 public:
     static std::set<std::string> fileDependency(const SIExpression& expr);
+	static void fileReplace(SIExpression &expr, const filepath& search, const filepath& replace);
 
 };
 
 //____________________________________________________________________
 // ------------------------------------------------------------------
 /*!
- * \brief Special evaluator: read an expression, return the expression definition as a string
+ * \brief Evaluate an IExpression and return a list of the ressources used in it
  */
 class ExprFileDependency: public constEvaluator{
 
@@ -33,6 +34,42 @@ public:
 
 protected:
 	std::set<std::string> fDependency;
+};
+
+//____________________________________________________________________
+// ------------------------------------------------------------------
+/*!
+ * \brief Search for parameters and replace them with a new value
+ */
+class ExprSearchReplacebase: public evaluator{
+
+public:
+	virtual const std::string eval(IExprOperator *arg,	IExprArgBase *){
+		arg->arg1()->accept(this);
+		arg->arg2()->accept(this);
+		return "";
+	}
+	virtual const std::string eval(const filepath &,	IExprArgBase *){return "";}
+	virtual const std::string eval(const std::string &,	IExprArgBase *){return "";}
+	virtual const std::string eval(const itladdress &,	IExprArgBase *){return "";}
+	virtual const std::string eval(const iexpression &,	IExprArgBase *){return "";}
+
+protected:
+	ExprSearchReplacebase(): evaluator() {}
+
+};
+
+template <typename T>
+class ExprSearchReplace: public ExprSearchReplacebase{
+	const T fSearch, fReplace;
+
+public:
+	static void searchReplace(SIExpression &expression, T search, T replace);
+	virtual const std::string eval(const T& value, IExprArgBase *exprArg);
+
+protected:
+	ExprSearchReplace(T search, T replace): ExprSearchReplacebase(), fSearch(search), fReplace(replace){}
+
 };
 
 } // End namespace
