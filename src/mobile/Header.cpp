83254622b6be5b343a067d479a9e7831e77f78b3
D. Fober <fober@grame.fr>
2016-01-15 15:33:33 +0100

    resize mode changed workaround showEvent removed
    Former-commit-id: 507a074bff630a8d1bdd56c47ab2bfc1b52a161f

--- src/mobile/Header.cpp
+++ src/mobile/Header.cpp
@@ -1,3 +1,28 @@
+/*
+
+  INScore Project
+
+  Copyright (C) 2015  Grame
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+  Grame Research Laboratory, 9 rue du Garet, 69001 Lyon - France
+  research@grame.fr
+
+*/
+
 #include "Header.h"
 #include <QtQuick>
 #include <QtQml>
@@ -8,12 +33,12 @@ namespace inscore {
 
 Header::Header(QWidget *parent) : QQuickWidget(parent)
 {
-	setResizeMode(QQuickWidget::SizeRootObjectToView);
+	setResizeMode(QQuickWidget::SizeViewToRootObject);
 	setSource(QUrl("qrc:///qml/header.qml"));
 
-	connect(rootObject(), SIGNAL(nextPanel()), this, SIGNAL(nextPanel()));
-	connect(rootObject(), SIGNAL(previousPanel()), this, SIGNAL(previousPanel()));
-	connect(rootObject(), SIGNAL(switchToPanel(QString)), this, SIGNAL(switchToPanel(QString)));
+//	connect(rootObject(), SIGNAL(nextPanel()), this, SIGNAL(nextPanel()));
+//	connect(rootObject(), SIGNAL(previousPanel()), this, SIGNAL(previousPanel()));
+//	connect(rootObject(), SIGNAL(switchToPanel(QString)), this, SIGNAL(switchToPanel(QString)));
 	connect(rootObject(), SIGNAL(popupMenu()), this, SIGNAL(popupMenu()));
 
 	QStringList l;
@@ -32,21 +57,21 @@ void Header::panelNameListChanged(QStringList nameList)
 	rootObject()->setProperty("titles", QVariant::fromValue(nameList));
 }
 
-void Header::showEvent(QShowEvent *e)
-{
-	QQuickWidget::showEvent(e);
-
-	///Workaround a QQmlWidget bug where the qml item is resize with a wrong height
-	///when showing the header without triggering a resizeEvent() (for exemple when exiting fullscreen mode)
-	///This subclass of showEvent can be removed if the qt bug is fixed...
-	if(isVisible()){
-		QTimer::singleShot(10, [this](){
-			int h = height();
-			setFixedHeight(h+1);
-			setFixedHeight(h);
-		});
-	}
-}
+//void Header::showEvent(QShowEvent *e)
+//{
+//	QQuickWidget::showEvent(e);
+//
+//	///Workaround a QQmlWidget bug where the qml item is resize with a wrong height
+//	///when showing the header without triggering a resizeEvent() (for exemple when exiting fullscreen mode)
+//	///This subclass of showEvent can be removed if the qt bug is fixed...
+//	if(isVisible()){
+//		QTimer::singleShot(10, [this](){
+//			int h = height();
+//			setFixedHeight(h+1);
+//			setFixedHeight(h);
+//		});
+//	}
+//}
 
 }//end namespace
 
