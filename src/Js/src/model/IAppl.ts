94e8bb70d52b0b6b6c95f61bee86a5fc52c86aca
Arnaudmar <arnaud.margaillan@gmail.com>
2016-04-22 17:09:30 +0200

    first version of Proxy
    Former-commit-id: 3060c0d19bd6cad2406038799738fe53e7759518

--- src/Js/src/model/IAppl.ts
+++ src/Js/src/model/IAppl.ts
@@ -1,8 +1,13 @@
 ///<reference path="IObject.ts"/>
 
 class IAppl extends IObject {
+ 
+    protected kApplType: string;
     
     constructor() {
         super('ITL');
+        this.kApplType = 'IAppl';
+        this.fTypeString = 'IAppl'; 
+
     } 
 }
