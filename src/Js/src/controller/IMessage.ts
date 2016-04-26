e35bd56e28d1e065ca32ccb6d64e28d20eb2d9c0
D. Fober <fober@grame.fr>
2016-04-26 12:49:15 +0200

    new IMessage implementation
    Former-commit-id: bab78b31c979c39168284ad9d965722f2ded956f

--- src/Js/src/controller/IMessage.ts
+++ src/Js/src/controller/IMessage.ts
@@ -1,14 +1,27 @@
 
+"use strict"
+
+class MsgParamRetVal<T> {
+    correct: boolean;
+    value: T;
+    constructor(check: boolean, val? : T) { this.correct = check; this.value = val; }
+}
+
+
 class IMessage {
  
     protected fAddress: string;
     protected fArguments: Array<any>;
-    
-    constructor(input: string|IMessage, params? : Array<any>) {
+   
+    constructor(input: string|IMessage, params? : Array<any>|string) {
         //if input is an adress
         if (typeof input === 'string') {
             this.fAddress = input; 
-            this.fArguments = params ? params : new Array<any>();
+            if (params) {
+        		if (typeof params === 'string')	this.fArguments = new Array<any>(params);
+            	else							this.fArguments = params;
+            }
+            else this.fArguments = new Array<any>();
         } 
         
         if (typeof input === 'object') {
@@ -17,43 +30,44 @@ class IMessage {
         }   
     }   
    
-    add(val: any): void { this.fArguments.push(val); };
-    
-    address(): string { return this.fAddress; }
-    
-    message(): string { 
-        var out = this.paramStr(0);
-        if (out.verif) { return out.param ; }
+    // add a value to the parameters
+    add(val: any): void 	{ this.fArguments.push(val); };    
+    // add values to the parameters; arrays are checked and pushed as values
+    push(val: any): void 	{
+    	if (Array.isArray(val)) {
+    		for (var i=0; i<val.length; i++) this.add (val[i]);
+    	}
+    	else this.fArguments.push(val); 
+    };
+    address(): string 		{ return this.fAddress; }
+    param(i: number): any 	{ return this.fArguments[i]; }
+    params(): Array<any> 	{ return this.fArguments; }
+
+    paramStr(i: number): MsgParamRetVal<string> { 
+    	return new MsgParamRetVal<string>(this.isType(i, "string"), this.fArguments[i]);
     }
+    paramNum(i: number): MsgParamRetVal<number> { 
+    	return new MsgParamRetVal<number>(this.isType(i, "number"), this.fArguments[i]);
+    }
+    paramArray(i: number): MsgParamRetVal<Array<any>> { 
+    	return new MsgParamRetVal<Array<any>>(this.isType(i, "object"), this.fArguments[i]);
+    }
+    
+    message(): MsgParamRetVal<string> 	{ return this.paramStr(0); }
+    
+    isType(i: number, type: string): boolean 	{ return (typeof this.fArguments[i] === type); }   
+    isString(i: number): boolean 	{ return this.isType(i, "string"); }
+    isNumber(i: number): boolean 	{ return this.isType(i, "number"); }
+    isArray(i: number): boolean 	{ return this.isType(i, "object"); }
 
-    param(i: number): string { return this.fArguments[i] }
-    
-    
-    paramStr(i: number): { verif: boolean, param: string } { 
-    	if (this.isString(i)) {
-    		var param = this.fArguments[i];
-    		return { verif: true, param: param };
-    	}
-    	return { verif: false, param: param };
-    } 
-    paramNum(i: number): { verif: boolean, param: number } { 
-    	if (this.isNumber(i)) {
-    		var param = this.fArguments[i];
-    		return { verif: true, param: param };
-    	}
-    	return { verif: false, param: param };
-    } 
-    paramArray(i: number): { verif: boolean, param: Array<any> } { 
-    	if (this.isArray(i)) {
-    		var param = this.fArguments[i];
-    		return { verif: true, param: param };
-    	}
-    	return { verif: false, param: param };
-    } 
-    
-    isString(i: number): boolean 	{ return (typeof this.fArguments[i] === "string") ? true : false; }   
-    isNumber(i: number): boolean 	{ return (typeof this.fArguments[i] === "number") ? true : false; }   
-    isArray(i: number): boolean 	{ return (typeof this.fArguments[i] === "object") ? true : false; }   
-
-   	size(): number 					{ return this.fArguments.length; }    
+   	size(): number 					{ return this.fArguments.length; }
+   	toString(): string	{
+   		var out: string = this.fAddress;
+   		for (var i: number=0; i < this.fArguments.length; i++) {
+   			if (this.isType(i, "object"))
+	   			out += " ( " + this.fArguments[i].toString() + " )";
+   			else out += " " + this.fArguments[i].toString();
+   		}
+   		return out;
+   	}  
 }
