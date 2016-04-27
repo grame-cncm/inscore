001861df67cc6973d91a43676e55f9d6e629b805
D. Fober <fober@grame.fr>
2016-04-27 15:56:42 +0200

    new abstract view objects
    Former-commit-id: 5502cbd8aa4c6ccac271f75457698ebf5b20bb0f

--- src/Js/src/view/VObjectView.ts
+++ src/Js/src/view/VObjectView.ts
@@ -2,84 +2,36 @@
 
 abstract class VObjectView {
     
-    protected fMotherScene: HTMLDivElement;
-    protected fObject: IObject;
-    protected fScene: HTMLDivElement;
+	abstract updateView			( obj: IObject) 		: void;
+	abstract updateObjectSize	( obj: IObject ) 		: void;
 
-    
-    constructor(obj: IObject, scene: HTMLDivElement, motherScene?: HTMLDivElement) {
-        this.fObject = obj;
-        this.fScene = scene; 
-        this.fMotherScene = scene;
-        
-        // Si ce n'est pas une scene, on renseigne la scene mère
-        if (motherScene) { this.fMotherScene = motherScene; }       
-    }
-    
-    getScene(): HTMLDivElement { return this.fScene }   
-    getMotherScene(): HTMLDivElement { return this.fMotherScene }
-    
-    
-    
-    updateView(): void {
-        var svg = this.fScene.firstElementChild.firstElementChild;
-        var obj = this.fObject;
-        
-        switch (svg.nodeName) {
-            case 'ellipse':
-                //svg.setAttribute('cx', obj.getPos().getXPos()+'');
-                //svg.setAttribute('cy', obj.getPos().getYPos()+'');
-                //svg.setAttribute('rx', obj.getPos().getWidth()+'');
-                //svg.setAttribute('ry', obj.getPos().getHeight()+'');     
-                break;
-                
-            case 'rect':
-                //svg.setAttribute('x', obj.getPos().getXPos()+'');
-                //svg.setAttribute('y', obj.getPos().getYPos()+'');
-                //svg.setAttribute('height', obj.getPos().getWidth()+'');
-                //svg.setAttribute('width', obj.getPos().getHeight()+'');
-                break;
-                
-            case 'curve':
-                //svg.setAttribute('d', 'M50,10 q50,10 50,200 t50,100');
-                //svg.setAttribute('stroke', '#1F56D2');
-                break;
-                
-            case 'line':
-                //svg.setAttribute('x1', '10');
-                //svg.setAttribute('y1', '10');
-                //svg.setAttribute('x2', '100');
-                //svg.setAttribute('y2', '100');
-                //svg.setAttribute('stroke', '#1F56D2')   
-                break;            
-        
-            default:
-                break;
-        }
-        
-        svg.setAttribute('style', "fill:rgba(" + obj.getColor().getR() + ', ' 
-                                                        + obj.getColor().getG() + ', ' 
-                                                        + obj.getColor().getB() + ', '
-                                                        + obj.getColor().getA() + ")");
+	// Maps the IObject [-1,1] y coordinate to the referenceRect().
+	abstract relative2SceneY(y: number, item: any ) : number;
 
-        this.fObject.setState(state.kClean);      
-    }
-    
-    updateObjectSize(object: IObject): void {}
+	// \brief Maps the IObject [-1,1] x coordinate to the referenceRect().
+	abstract relative2SceneX(x: number, item: any ) : number;
+
+	// \brief Maps the IObject [0,2] width value to the corresponding referenceRect() value.
+	abstract relative2SceneWidth(width: number, item: any ) : number;
+
+	// \brief Maps the IObject [0,2] height value to the corresponding referenceRect() value.
+	abstract relative2SceneHeight(height: number, item: any ) : number;
+
+	// \brief Maps the referenceRect() width value to the corresponding [0,2] value.
+	abstract scene2RelativeWidth(width: number, item: any ) : number;
+
+	// \brief Maps the referenceRect() height value to the corresponding [0,2] value.
+	abstract scene2RelativeHeight(height: number, item: any ) : number;
+
+	// \brief Maps the referenceRect() x value to the corresponding [-1,1] value.
+	abstract scene2RelativeX(x: number, item: any ) : number;
+
+	// \brief Maps the referenceRect() y value to the corresponding [-1,1] value.
+	abstract scene2RelativeY(y: number, item: any ) : number;
+
+	// \brief updates the local mapping (do nothing at IObject level) 
+//	abstract updateLocalMapping (obj: IObject ): void;
 		
-    setParentItem(parent: VObjectView) {}
-    
-   
-
-
-    /*relative2SceneY(y: number, item:QGraphicsItem  = 0): number {return 0};	
-    relative2SceneX(x: number, item:QGraphicsItem  = 0): number {return 0};
-    relative2SceneWidth(width: number, item:QGraphicsItem  = 0): number {return 0};
-    relative2SceneHeight(height: number, item:QGraphicsItem  = 0): number {return 0};
-
-    scene2RelativeWidth(width: number, item:QGraphicsItem  = 0): number {return 0};
-    scene2RelativeHeight(height: number, item:QGraphicsItem  = 0): number {return 0};
-    scene2RelativeX(x: number, item:QGraphicsItem  = 0): number {return 0};
-    scene2RelativeY(y: number, item:QGraphicsItem  = 0): number {return 0};
-    */
+	// \brief initialize an object view (do nothing at IObject level)
+	abstract initialize (obj: IObject ): void
 }
