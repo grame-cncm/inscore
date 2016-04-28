2b352f327cd5453abe2013fd092d67974c390751
Arnaudmar <arnaud.margaillan@gmail.com>
2016-04-28 14:54:56 +0200

    set method implementation in IRectShape
    Former-commit-id: 1ad1a67f280cf6d1489681e8bb298d8174c6c811

--- src/Js/src/model/IRectShape.ts
+++ src/Js/src/model/IRectShape.ts
@@ -10,8 +10,34 @@ class IRectShape extends IObject {
     }
 
     create(name: string, parent: IObject): IRectShape { return new IRectShape(name, parent); }
-    
+
+   
+    set(msg: IMessage): msgStatus {
+        let status: msgStatus = super.set(msg);
+        
+        // Cas ou le type est différent, le proxy est utilisé dans super.set()
+        if (status & (msgStatus.kProcessed + msgStatus.kProcessedNoChange)) return status; 
+
+        // Vérification des paramêtres du "set"
+        if (msg.size() == 4) {
+            let width = msg.paramNum(2), height = msg.paramNum(3);
+            if (!width.correct || !height.correct) { return msgStatus.kBadParameters; }
+
+            // Si les valeurs des attributs ne sont pas les mêmes
+            if ( (width.value != this.fPosition.getWidth()) || (height.value != this.fPosition.getHeight()) ) {
+                this.fPosition.setWidth(width.value);
+                this.fPosition.setHeight(height.value);
+                this.newData(true);
+                console.log("L'objet a bien été modifié selon le message set : // IRectShape");
+                console.log(this);
+                status = msgStatus.kProcessed;
+            }
+            else status = msgStatus.kProcessedNoChange;
+        }
+        else status = msgStatus.kBadParameters;
+        return status;
+    }  
+   
     //accept (Updater*): void
-    // virtual MsgHandler::msgStatus set (const IMessage* msg);
-		
+    // virtual MsgHandler::msgStatus set (const IMessage* msg);		
 }
