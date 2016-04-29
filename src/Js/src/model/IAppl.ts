647329602624062fa3aa91dbaa1d466514af4e39
D. Fober <fober@grame.fr>
2016-04-29 17:10:13 +0200

    IAppl implements processMsg and getSet
    Former-commit-id: cb36d02daaee495dc8a9ae7d80b1a1c946d40be7

--- src/Js/src/model/IAppl.ts
+++ src/Js/src/model/IAppl.ts
@@ -1,17 +1,51 @@
+///<reference path="../controller/THandlersPrototypes.ts"/>
+///<reference path="../lib/OSCAddress.ts"/>
 ///<reference path="IObject.ts"/>
 ///<reference path="IProxy.ts"/>
 
 class IAppl extends IObject {
  
     protected kApplType: string;
+    protected fReceivedMsgs: number;
     
     constructor() {
         super('ITL');
         this.kApplType = 'IAppl';
-        this.fTypeString = 'IAppl'; 
+        this.fTypeString = 'IAppl';
+        this.fReceivedMsgs = 0;
     } 
 
-    getAppl() : IObject		{ return this; }
+    getAppl() : IObject					{ return this; }
+    getSet(address: string): IMessage	{ return new IMessage(address); }
+
+    processMsg (address: string, addressTail: string , msg: IMessage): number {
+    	this.fReceivedMsgs++;
+		let status = msgStatus.kBadAddress;
+		let head = address;
+		let tail = addressTail;
+/*
+		SIMessage msg = IMessage::create (*imsg);
+		TAliasesMap::const_iterator i = fAliases.find(imsg->address());
+		if (i != fAliases.end()) {
+			msg->setAddress (i->second.first);
+			if (i->second.second.size()) 
+				msg->setMessage(i->second.second);
+			head = OSCAddress.addressFirst(i->second.first);
+			tail = OSCAddress.addressTail(i->second.first);
+		}
+*/
+		if (tail.length) 			// application is not the final destination of the message
+			status = super.processMsg(head, tail, msg);
+	
+//		else if (match(head)) {		// the message is for the application itself
+		else if (head === "ITL") {		// the message is for the application itself
+			status = this.execute(msg);
+			if (status & msgStatus.kProcessed)
+				this.setState(objState.kModified);
+		}
+//		if ((status == msgStatus.kProcessed) || (status == msgStatus.kProcessedNoChange))
+			return status;
+    }
 
 	protected newObj (msg: IMessage, name: string): { status: msgStatus, obj?: IObject } 
     				{ return this.proxy_create(msg, name, this); }                
