d53b566fc042d62e4c5c4d679ad087b84b3421ad
D. Fober <fober@grame.fr>
2016-05-03 11:46:41 +0200

    glue implementation (in progress)
    Former-commit-id: 7e5cabbd821cf40dc4873cc90c658172afe8bd88

--- src/Js/src/controller/IGlue.ts
+++ src/Js/src/controller/IGlue.ts
@@ -1,15 +1,40 @@
 
+///<reference path="IMessage.ts"/>
+///<reference path="../inscore.ts"/>
 ///<reference path="../model/IAppl.ts"/>
-///<reference path="../events/dragAndDrop.ts"/>
+///<reference path="../events/documentEvents.ts"/>
 
-class IGlue {
+interface TTimerTask  { (): void; }
+
+class IGlue { 
 	protected fAppl: IAppl;
+	protected fTimer: number
 	
-    constructor() 			{ this.fAppl = new IAppl(); }
-	getRoot() : IAppl		{ return this.fAppl; }
+    constructor() 			{ 
+    	this.fAppl = new IAppl(); 
+    	let inscore = new INScore(this.fAppl);
+    }
+//	getRoot() : IAppl		{ return this.fAppl; }
 
-    initDragAndDrop(): void {
+    initEventHandlers(): void {
 		document.addEventListener("drop", dropEvent, false);
 		document.addEventListener("dragover", dragOverEvent, false);
+		window.addEventListener("resize", resizeDocument);
     }
+    
+    start(): void {
+    	INScore.postMessage ("/ITL/scene", ["new"]);
+    	this.fTimer = setTimeout (this._timetask(), this.fAppl.getRate()) ;		
+    }
+
+    stop(): void {
+    	INScore.postMessage ("/ITL", ["quit"]);
+    	clearTimeout (this.fTimer) ;		
+    }
+
+	timetask() : void {
+		console.log ("timetask called ");
+    	this.fTimer = setTimeout (this._timetask(), this.fAppl.getRate()) ;		
+	}
+    _timetask()	: TTimerTask 		{ return () => this.timetask(); };
 }
