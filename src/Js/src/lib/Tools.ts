91a3af1bea56bcff4016351eb9fa036924b49ce0
D. Fober <fober@grame.fr>
2016-05-10 14:39:55 +0200

    regexp predicat correction
    Former-commit-id: f72d29a2ee8f8174f44af6018015641744e0e72c

--- src/Js/src/lib/Tools.ts
+++ src/Js/src/lib/Tools.ts
@@ -3,8 +3,9 @@ class Tools {
     static regexp (str: string): boolean
     {
         let n = str.length;
-        for (let i: number = 0; i < n; i++) {
-            if ((str[i] == '*') || (str[i] == '?') || (str[i] == '['))
+        for (let i = 0; i < n; i++) {
+        	let c = str[i];
+            if ((c == '*') || (c == '?') || (c == '[') || (c == '{'))
                 return true;
         }
         return false;
