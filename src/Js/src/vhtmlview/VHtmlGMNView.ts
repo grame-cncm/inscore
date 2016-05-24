2f00a4a7a15a1800bdeab6a04e2da4427008f396
D. Fober <fober@grame.fr>
2016-05-24 10:10:16 +0200

    gmn view in progress
    Former-commit-id: 455bf3809f243a6b9fa587c9df5be043cdcd8f34

--- src/Js/src/vhtmlview/VHtmlGMNView.ts
+++ src/Js/src/vhtmlview/VHtmlGMNView.ts
@@ -14,33 +14,24 @@ class VHtmlGMNView extends VHtmlSvg {
     }
       
     updateView	(obj: IGuidoCode) : void {           	
-        this.updateGMN(obj);
+        this.fSVG.innerHTML = this.updateGMN(obj);
+        let elt = this.getHtml();
+    	this.updateSvgSize (elt.clientWidth*2, elt.clientHeight*2);   
 		this.updateObjectSize (obj);
-
+/*
+        elt.style.height = "auto";
+        elt.style.width = "auto";                
+*/
+/*
         let scale = obj.fPosition.getScale();
 		let w = this.relative2SceneWidth( obj.fPosition.getWidth() ) * scale;
 		let h = this.relative2SceneHeight( obj.fPosition.getHeight() ) * scale;
-    	this.updateSvgSize (w, h);   
-        
         console.log('w : ' + w + ' / h : ' + h + ' / scale : ' + scale);
-        //console.log(obj.fPosition.getWidth());
-        
-        this.fSVG.setAttribute('width', w.toString());
-        this.fSVG.setAttribute('height', h.toString()); 
-		let rx = w/2;
-	    let ry = h/2;
-        this.fSVG.setAttribute('cx', rx.toString());
-        this.fSVG.setAttribute('cy', ry.toString());
-        this.fSVG.setAttribute('rx', rx.toString());
-        this.fSVG.setAttribute('ry', ry.toString());
-        let elt = this.getHtml();
-        elt.style.height = "auto";
-        elt.style.width = "auto";                
-
+*/        
 		super.updateView(obj);
 	}
        
-    updateGMN(obj: IGuidoCode) {
+    updateGMN(obj: IGuidoCode) : string {
         let div = this.getHtml();
         let gmn = obj.getGMN();
         
@@ -51,13 +42,22 @@ class VHtmlGMNView extends VHtmlSvg {
 	    guidoEngine.closeParser(p);
         
         let gr = guidoEngine.ar2gr(ar);
-	    let result = guidoEngine.gr2SVG(gr, 1, true, 0);
+	    let result = guidoEngine.gr2SVG(gr, 1, false, 0);
         
 	    guidoEngine.freeGR(gr);
 	    guidoEngine.freeAR(ar);    
-          
-        this.fSVG.innerHTML = result;
+        return result;
     }  
+
+	_updateView	( obj: IGuidoCode) : RefreshMethod { return () => this.updateView (obj); }
+
+	updateObjectSize ( obj: IGuidoCode) : void {
+        let w = this.scene2RelativeWidth(this.fSVG.clientWidth);
+        let h = this.scene2RelativeHeight(this.fSVG.clientHeight);
+		obj.fPosition.setWidth (w);
+		obj.fPosition.setHeight (h);
+		if (!w || !h)  setTimeout (this._updateView(obj), 50) ;		
+	}
 }
 
 /*
