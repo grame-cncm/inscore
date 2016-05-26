d0279b071c8b7acbe4a47dbf6ac7cd4266ab197b
Arnaudmar <arnaud.margaillan@gmail.com>
2016-05-26 16:02:22 +0200

    add regexp to check if the file name is correct
    Former-commit-id: 5b43b0cfaf8142c81cf20411f6f9e4926aae7c94

--- src/Js/src/events/documentEvents.ts
+++ src/Js/src/events/documentEvents.ts
@@ -68,11 +68,31 @@ function resizeDocument() { INScore.postMessage ("/ITL/*", ["redraw"]); }
 function getFileProperties(file: string): { name: string, ext: string } {
 	let ext 	= file.substring(file.lastIndexOf('.')+1, file.length);
 	let name 	= file.substring(0, file.lastIndexOf('.'));
+	name = buildCorrectName(name);
 	return { name: name, ext: ext }	
 }
 
+function buildCorrectName(name: string): string {
+	let myRegex = /^[a-zA-Z-_][-_a-zA-Z0-9]+$/.test(name);
+	if (!myRegex) {
+		let first = name.substring[0, 1];
+		let myRegex = /^[a-zA-Z-_]$/.test(first);
+		if (!myRegex) {
+			name = name.substring(1, name.length)
+		}
+		for (let i =1; i < name.length; i++ ) {
+		let myRegex = /^[-_a-zA-Z0-9]$/.test(name[i]);
+		if (!myRegex) {
+			name = name.replace(name[i], "")
+		}	
+		}			
+	}	
+	console.log(name);
+	return name	
+}
 
-
+// controller le nom avec regExp
+// 
 
 
 
