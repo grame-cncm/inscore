fa30e8b8fdcf98d7d44e3152bd1898d8f6705aee
D. Fober <fober@grame.fr>
2016-06-02 11:07:39 +0200

    code to extend the DOM
    Former-commit-id: 088d88a94d1c6f2bb43f7dbb40640dccc2b1ac45

---
+++ src/Js/html/inscore-code.js
@@ -0,0 +1,24 @@
+
+	function initComponent() {
+		// Create a prototype for a new element that extends HTMLElement
+		var inscoreProto = Object.create(HTMLElement.prototype);
+
+		// Setup our Shadow DOM and clone the template for each element guido-viewer created
+		inscoreProto.createdCallback = function() {
+			root = this.createShadowRoot();
+			processINScore (this.textContent);
+		};
+
+		var inscore = document.registerElement('inscore-code', {'prototype': inscoreProto});
+	}
+	
+	function processINScore(text) {
+		let loader = new TILoader;
+		loader.process (text, INScore.getRoot());
+//		INScore.postMessage ("/ITL/scene", ["redraw"]);
+	}
+	
+	function start() {
+		StartINScore();
+		initComponent();
+	};
