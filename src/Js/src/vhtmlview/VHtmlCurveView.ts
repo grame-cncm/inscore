4845ded16894b3f6904c498c50f73a7e6e59c037
Arnaudmar <arnaud.margaillan@gmail.com>
2016-07-04 12:52:53 +0200

    Curve view script updtate
    Former-commit-id: 419d6a7805d56109c1e1489dae0c330dd4785c37

--- src/Js/src/vhtmlview/VHtmlCurveView.ts
+++ src/Js/src/vhtmlview/VHtmlCurveView.ts
@@ -32,23 +32,35 @@ class VHtmlCurveView extends VHtmlSvg {
                                         + 't' + d1 + ',' + d2);
         }
 
-        let scale = obj.fPosition.getScale();        
-        let w = this.relative2SceneWidth( obj.fPosition.getWidth() ) * scale;
-		let h = this.relative2SceneHeight( obj.fPosition.getHeight() ) * scale;
-    	this.updateSvgSize (w, h);
-        
-        
-        
-        
-        
-        
-        
+            let curveSize = this.fCurve.getBBox();
+            this.updateSvgPos (curveSize.x, curveSize.y);  
+            this.updateSvgSize (curveSize.width, curveSize.height);
+       
+        for (let i = 0; i < BezierCurves.length; i++) {
+            let points: Array<number> = BezierCurves[i].points()
+            let a1 = this.relative2SceneX(points[0]) - curveSize.x;
+            let a2 = this.relative2SceneY(points[1]) - curveSize.y;
+            let b1 = this.relative2SceneX(points[2]) - curveSize.x;
+            let b2 = this.relative2SceneY(points[3]) - curveSize.y;
+            let c1 = this.relative2SceneX(points[4]) - curveSize.x;
+            let c2 = this.relative2SceneY(points[5]) - curveSize.y;
+            let d1 = this.relative2SceneX(points[6]) - curveSize.x;
+            let d2 = this.relative2SceneY(points[7]) - curveSize.y;
+
+            this.fCurve.setAttribute('d', 'M' + a1 + ',' + a2 + ' '
+                                        + 'q' + b1 + ',' + b2 + ' '    
+                                              + c1 + ',' + c2 + ' ' 
+                                        + 't' + d1 + ',' + d2);
+        }        
+            this.updateObjectSize (obj);
+            this.fCurve.style.fill = obj.fColor.getRGBString();        
+            
       /*  
         let scale = obj.fPosition.getScale();
 		let w = this.relative2SceneWidth( obj.fPosition.getWidth() ) * scale;
 		let h = this.relative2SceneHeight( obj.fPosition.getHeight() ) * scale;
     	this.updateSvgSize (w, h);
-     */   
+      
         this.fCurve.setAttribute('width', w.toString());
         this.fCurve.setAttribute('height', h.toString());
         this.fCurve.style.fill = obj.fColor.getRGBString();
@@ -59,6 +71,26 @@ class VHtmlCurveView extends VHtmlSvg {
         //this.fCurve.setAttribute('rx', rx.toString());
         //this.fCurve.setAttribute('ry', ry.toString());
 		this.updatePos(obj);
-   
-	}     
+    */ 
+	}  
+
+	updateSvgPos (x: number, y: number): void { 
+        console.log(x+ ' '+y)
+    	let elt = this.getHtml();		
+		elt.style.left = x +"px";
+        elt.style.top  = y +"px";
+		this.fSVG.style.left = x +"px";
+        this.fSVG.style.top  = y +"px";
+	}  
+
+	updateObjectSize ( obj: IObject) : void {
+        let w = this.scene2RelativeWidth(this.fSVG.clientWidth);
+        let h = this.scene2RelativeHeight(this.fSVG.clientHeight);
+        let x = this.scene2RelativeHeight(this.fSVG.clientLeft);
+        let y = this.scene2RelativeHeight(this.fSVG.clientTop);
+		obj.fPosition.setWidth (w);
+		obj.fPosition.setHeight (h);
+		obj.fPosition.setXPos (x);
+		obj.fPosition.setYPos (y);
+    }	            
 }
