0223fea86b67ff113d08e0fcd3a51d061fd92dd5
D. Fober <fober@grame.fr>
2016-10-06 18:33:34 +0200

    video is making use of the new media player (shared with audio)
    Former-commit-id: d33f9264339657696853864eeab891cf2ad63923

--- src/inscore/view/VVideoView.cpp
+++ src/inscore/view/VVideoView.cpp
@@ -40,105 +40,53 @@ namespace inscore
 
 //----------------------------------------------------------------------
 VVideoView::VVideoView(QGraphicsScene * scene, const IVideo* video)
- :	VGraphicsItemView( scene , new IQGraphicsVideoItem(video) )
-#ifndef USEPHONON
-	, fMediaPlayer(0, QMediaPlayer::VideoSurface)
-	, fVideo(0), fReady(0)
-#endif
+ :	VGraphicsItemView( scene , new IQGraphicsVideoItem(video) ), fVideo(0)
 {
 	fVideoItem = (IQGraphicsVideoItem*)(fItem);
-#ifndef USEPHONON
-    connect(&fMediaPlayer, SIGNAL(error(QMediaPlayer::Error)), this, SLOT(error(QMediaPlayer::Error)));
-    connect(&fMediaPlayer, SIGNAL(mediaStatusChanged(QMediaPlayer::MediaStatus)), this, SLOT(mediaStatusChanged(QMediaPlayer::MediaStatus)));
-    connect(&fMediaPlayer, SIGNAL(stateChanged(QMediaPlayer::State)), this, SLOT(stateChanged(QMediaPlayer::State)));
-    connect(&fMediaPlayer, SIGNAL(seekableChanged(bool)), this, SLOT(seekableChanged(bool)));
-    connect(&fMediaPlayer, SIGNAL(positionChanged(qint64)), this, SLOT(positionChanged(qint64)));
-    connect(&fMediaPlayer, SIGNAL(durationChanged(qint64)), this, SLOT(durationChanged(qint64)));
     connect(fVideoItem, SIGNAL(nativeSizeChanged(const QSizeF &)), this, SLOT(nativeSizeChanged(const QSizeF &)));
-#endif
+	player()->setVideoOutput (fVideoItem);
+}
 
+
+//----------------------------------------------------------------------
+void VVideoView::mediaReady()
+{
+	fVideo->setMediaDuration(player()->duration());
+	fVideo->videoReady();
 }
 
 //----------------------------------------------------------------------
-#ifndef USEPHONON
-void VVideoView::error(QMediaPlayer::Error err )
+void VVideoView::mediaEnd()					{ fVideo->videoEnd(); }
+void VVideoView::posChanged(qint64 pos)		{ fVideo->setIDate(pos); }
+
+//----------------------------------------------------------------------
+void VVideoView::error(QString msg)
 {
-	ITLErr << (fVideo ? fVideo->getOSCAddress().c_str() : "no video OSC address") << "error" << err << fMediaPlayer.errorString().toStdString() << ITLEndl;
+	ITLErr << (fVideo ? fVideo->getOSCAddress().c_str() : "no video OSC address") << "error" << msg.toStdString() << ITLEndl;
 }
 
 //----------------------------------------------------------------------
-void VVideoView::nativeSizeChanged(const QSizeF & size)
+void VVideoView::sizeChanged(const QSizeF & size)
 {
-	if (size.isEmpty()) return;
-//qDebug() << "VVideoView::nativeSizeChanged " << size;
 	fVideoItem->setSize (size);
 	fVideo->setWidth ( scene2RelativeWidth(size.width()) );
 	fVideo->setHeight( scene2RelativeHeight(size.height()) );
 	INScore::postMessage (fVideo->getOSCAddress().c_str(), kx_GetSetMethod, fVideo->getXPos());
-	if (++fReady == 2) fVideo->videoReady();
 }
-
-//----------------------------------------------------------------------
-void VVideoView::seekableChanged(bool /*seekable*/)		{ /* qDebug() << "VVideoView::seekableChanged :" <<  seekable;*/ }
-void VVideoView::positionChanged(qint64 pos)			{ fVideo->setIDate(pos); }
-void VVideoView::durationChanged(qint64 duration)		{ fVideo->setVideoDuration (duration); if (++fReady == 2) fVideo->videoReady();}
-void VVideoView::stateChanged (QMediaPlayer::State /*state*/)	{ /*qDebug() << "VVideoView::stateChanged :" <<  state;*/ }
-
-//----------------------------------------------------------------------
-void VVideoView::mediaStatusChanged (QMediaPlayer::MediaStatus status)
-{
-//qDebug() << "VVideoView::mediaStatusChanged" << status;
-	switch (status) {
-		case QMediaPlayer::LoadedMedia:			// this is necessary to trigger the nativeSizeChanged slot
-			fMediaPlayer.play();
-			fMediaPlayer.setPosition(0);
-			fMediaPlayer.pause();
-			break;
-			
-		case QMediaPlayer::BufferedMedia:
-		case QMediaPlayer::UnknownMediaStatus:
-			break;
-		case QMediaPlayer::InvalidMedia:
-			ITLErr << (fVideo ? fVideo->getOSCAddress().c_str() : "no video OSC address") << "invalid media" << ITLEndl;
-			break;
-
-		case QMediaPlayer::EndOfMedia:
-			fMediaPlayer.setPosition( fMediaPlayer.duration()-1 );
-			fVideo->videoEnd();
-			break;
-		default:
-			break;
-	}
-}
-#endif
 
 //----------------------------------------------------------------------
 void VVideoView::initFile( IVideo * video, const QString&  videoFile )
 {
-#ifdef USEPHONON
-	fVideoItem->setMediaFile( videoFile );
-	fVideoItem->media()->play();
-	fVideoItem->media()->pause();	
-	updateObjectSize (video);
-#else
-	fReady = 0;
-	fVideo = video;
-	fMediaPlayer.stop ();
-	fMediaPlayer.setMedia(QUrl::fromLocalFile(videoFile));
-	fMediaPlayer.setVideoOutput (fVideoItem);
-	fMediaPlayer.setNotifyInterval(10);
-
+	setFile(videoFile);
 	fVideoItem->setAspectRatioMode(Qt::IgnoreAspectRatio);
-
 	video->setWidth(0.f);		// unknown width
 	video->setHeight(0.f);		// unknown height
-//    connect(fVideoItem, SIGNAL(nativeSizeChanged(const QSizeF &)), this, SLOT(nativeSizeChanged(const QSizeF &)));
-#endif
 }
 
 //----------------------------------------------------------------------
 void VVideoView::initialize( IVideo * video  )
 {
+	fVideo = video;
 	QString file = VApplView::toQString( video->getPath().c_str() );
 	if ( QFile::exists(  file  ) ) 
 		initFile (video, file);
@@ -150,32 +98,13 @@ void VVideoView::updateView( IVideo * video  )
 	video->cleanupSync();
 	fVideoItem->setOpacity (video->getA() / 255.f);
 	QSizeF size ( relative2SceneWidth(video->getWidth()),relative2SceneHeight(video->getHeight()));
-	qint64 pos = video->vDate();
-		
-#ifdef USEPHONON
-	fVideoItem->resize( size );
-	fVideoItem->media()->seek( pos );
-#else
-	if (pos < 0 ) pos = 0;
-	if (pos > fMediaPlayer.duration()) pos = fMediaPlayer.duration()-1;
 	if (size != fVideoItem->size()) fVideoItem->setSize( size );
+	
+	updatePlayer (video);
 
-	fMediaPlayer.setVolume( video->volume() * 100);
-	if (video->rateModified()) fMediaPlayer.setPlaybackRate( video->rate() );
-	if (video->playing()) {
-		if (fMediaPlayer.state() !=  QMediaPlayer::PlayingState) {
-			fMediaPlayer.play ();
-		}
-	}
-	else if (fMediaPlayer.state() ==  QMediaPlayer::PlayingState)
-		fMediaPlayer.pause ();
-
-	if (video->vdateModified())
-		fMediaPlayer.setPosition( pos );
-#endif
 	itemChanged();
 	VGraphicsItemView::updateView( video );
 }
 
 
-} // end namespoace
+} // end namespace
