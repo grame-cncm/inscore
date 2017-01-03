93603890edacc82b2e39d5bc4553f808657edbf1
D. Fober <fober@grame.fr>
2017-01-03 17:13:25 +0100

    update test tool for expressions parser
    Former-commit-id: 5d01016b8073a8daa01488d3c11adf57703e4f28

--- src/tests/expression/testparser.cpp
+++ src/tests/expression/testparser.cpp
@@ -1,23 +1,27 @@
 #include "IExprParser.h"
 #include <fstream>
 
+using namespace std;
 
 int main( int argc, const char* argv[] )
 {
-    std::ifstream ifs;
-    ifs.open("test.inscore", std::ifstream::in);
-    if(!ifs.is_open()){
-	std::cerr<<"test.inscore not found..."<<std::endl;
-	return -1;
-    }
+	for (int i=1; i<argc; i++) {
+		const char* arg = argv[i];
+		std::ifstream ifs;
+		cout << "check " << arg << ":" << endl;
+		ifs.open(arg, ifstream::in);
+		if(!ifs.is_open()){
+			cerr << arg << ": not found..." << endl;
+			return -1;
+		}
 
-    std::string data((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
-
-    inscore::SIExpression e;
-    if(!inscore::IExprParser::parseExpression(data, e)){
-	std::cout<<"Parsing of: \""<<data<<"\" failed."<<std::endl;
-	return -1;
-    }
-    std::cout<<e->rootNode()<<std::endl;
+		string data((istreambuf_iterator<char>(ifs)), istreambuf_iterator<char>());
+		inscore::SIExpression e;
+		if(!inscore::IExprParser::parseExpression(data, e)){
+			cerr << "Parsing of: \"" << data << "\" failed." << endl;
+			return -1;
+		}
+		cout << " => " << e << endl;
+	}
     return 0;
 }
