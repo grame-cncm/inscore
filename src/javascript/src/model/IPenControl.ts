2738c0c44b8901ef88ebae9eb8addec782bea6ad
D. Fober <fober@grame.fr>
2017-02-22 14:57:23 +0100

    penstyle arrays renamed
    Former-commit-id: 8e2dfdfdabb78d571d7cc73df69519caa692669b

--- src/javascript/src/model/IPenControl.ts
+++ src/javascript/src/model/IPenControl.ts
@@ -10,8 +10,8 @@ class IPenControl {
     fPenAlpha: number;
     protected fModified : boolean; 
     
-    fPenStylesList_set: { [id: string] : penStyle; } = {};
-    fPenStylesList_get: { [id: number] : string; } = {};
+    fPenStyleStr2Num: { [id: string] : penStyle; } = {};
+    fPenStyleNum2Str: { [id: number] : string; } = {};
     
     constructor(type: string) {
         this.fPenWidth = 0;
@@ -30,17 +30,17 @@ class IPenControl {
    	modify(): void 		{ this.fModified = true; }   
    
     buildPenStyle(): void {
-        this.fPenStylesList_set["solid"]         = penStyle.solid;
-        this.fPenStylesList_set["dash"]          = penStyle.dash;
-        this.fPenStylesList_set["dot"]           = penStyle.dot;
-        this.fPenStylesList_set["dashDot"]       = penStyle.dashDot;
-        this.fPenStylesList_set["dashDotDot"]    = penStyle.dashDotDot;
+        this.fPenStyleStr2Num["solid"]         = penStyle.solid;
+        this.fPenStyleStr2Num["dash"]          = penStyle.dash;
+        this.fPenStyleStr2Num["dot"]           = penStyle.dot;
+        this.fPenStyleStr2Num["dashDot"]       = penStyle.dashDot;
+        this.fPenStyleStr2Num["dashDotDot"]    = penStyle.dashDotDot;
 
-        this.fPenStylesList_get[penStyle.solid]         = "solid";
-        this.fPenStylesList_get[penStyle.dash]          = "dashed";
-        this.fPenStylesList_get[penStyle.dot]           = "dotted";
-        this.fPenStylesList_get[penStyle.dashDot]       = "dashDot";
-        this.fPenStylesList_get[penStyle.dashDotDot]    = "dashDotDot";        
+        this.fPenStyleNum2Str[penStyle.solid]         = "solid";
+        this.fPenStyleNum2Str[penStyle.dash]          = "dashed";
+        this.fPenStyleNum2Str[penStyle.dot]           = "dotted";
+        this.fPenStyleNum2Str[penStyle.dashDot]       = "dashDot";
+        this.fPenStyleNum2Str[penStyle.dashDotDot]    = "dashDotDot";        
     }
     
     getAlphaString(): string { return 'rgba(' + this.fPenColor.getR() + ', ' +
@@ -51,18 +51,19 @@ class IPenControl {
     
     getPenWidth(): number   { return this.fPenWidth; }
     getPenColor(): IColor   { return this.fPenColor; }
-    getPenStyle(): string   { return this.fPenStylesList_get[this.fPenStyle]; }
+    getPenStyle(): string   { return this.fPenStyleNum2Str[this.fPenStyle]; }
+    getPenStyleNum(): number { return this.fPenStyle; }
     getPenAlpha(): number   { return this.fPenAlpha; }
 
     _getPenWidth(): GetNumMethod    { return () => this.fPenWidth; }
     _getPenColor(): GetArrayMethod  { return () => this.fPenColor.getRGB(); }
-    _getPenStyle(): GetStringMethod    { return () => this.fPenStylesList_get[this.fPenStyle]; }
+    _getPenStyle(): GetStringMethod    { return () => this.fPenStyleNum2Str[this.fPenStyle]; }
     _getPenAlpha(): GetNumMethod    { return () => this.fPenAlpha; }
 
     setPenWidth(penWidth : number)  : void  { this.fPenWidth = penWidth; this.modify(); }
     setPenColor(penColor : IColor)  : void  { this.fPenColor = penColor; this.modify(); }
     setPenStyle(penStyle : string)  : void  { 
-        let style = this.fPenStylesList_set[penStyle]; 
+        let style = this.fPenStyleStr2Num[penStyle]; 
         if (!style && style != 0) { ITLError.badParameter("penStyle", penStyle);}
         else { this.fPenStyle = style; this.modify(); }
     }
