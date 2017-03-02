4cb10a66073eacc84b9c691deb891f51bf9a3cc4
D. Fober <fober@grame.fr>
2017-03-02 16:17:32 +0100

    fix polygon view: - adaptation to new design - computes scene points with points normalized to 0
    Former-commit-id: f96c28a1b10cc0e4f0d9a9c59b35f0c4992ea004

--- src/javascript/src/vhtmlview/VHtmlPolygonView.ts
+++ src/javascript/src/vhtmlview/VHtmlPolygonView.ts
@@ -12,96 +12,38 @@ class VHtmlPolygonView extends VHtmlSvg {
     	this.getHtml().className = "inscore-polygon";
     	this.fSVG.appendChild(this.fPolygon)
     }
+
+	getSize (obj: IObject):  {w: number, h: number } {
+        let size = this.fPolygon.getBBox();        
+		return { w: size.width, h: size.height };
+	}
     
     updateView	( obj: IObject) : void {
-    	super.updateView(obj);
-
         let polygon = <IPolygon>obj;        
         if (obj.isNewData()) {
-            obj.newData(false);
 		    let points = polygon.getPoints();
+            let minx = 0xffffffff;
+            let miny = 0xffffffff;
+            for (let i = 0; i < points.length; i++) {
+            	minx = Math.min(minx, points[i].getX());
+            	miny = Math.min(miny, points[i].getY());
+			}
             let strPoints: string = '';
             for (let i = 0; i < points.length; i++) {
-                let x = this.relative2SceneX( points[i].getX() );
-                let y = this.relative2SceneY( points[i].getY() );
-                if (!x) x = 1;
-                if (!y) y = 1;    
+                let x = this.relative2SceneWidth( points[i].getX() - minx );
+                let y = this.relative2SceneWidth( points[i].getY() - miny);
                 strPoints += x + ',' + y + ' ';
             }          
             this.fPolygon.setAttribute('points', strPoints);
-            
-            let polygonSize = this.fPolygon.getBBox();        
-//            this.updateSvgSize (polygonSize.width, polygonSize.height);            
-//            this.updateSvgPos(polygonSize.x, polygonSize.y);                          
-
-            if ( !this.getOrigin(polygonSize) ) {
-                let x = this.scene2RelativeWidth(polygonSize.width)
-                x = Math.floor(this.relative2SceneY(- x/2)*1000)/1000;
-                let y = this.scene2RelativeHeight(polygonSize.height)
-                y = Math.floor(this.relative2SceneX(- y/2)*1000/1000); 
-
-                let strPoints2: string = '';
-                for (let i = 0; i < points.length; i++) {
-                    let x = this.relative2SceneX( points[i].getX()) - polygonSize.x;
-                    let y = this.relative2SceneY( points[i].getY()) - polygonSize.y;
-                    if (!x) x = 1;
-                    if (!y) y = 1;    
-                    strPoints2 += x + ',' + y + ' ';
-                }
-                this.fPolygon.setAttribute('points', strPoints2);
-                
-                let elt = this.getHtml();
-                elt.setAttribute('position', 'absolute');
-                elt.style.top = y + 'px';
-                elt.style.left = x + 'px';
-                elt.style.width = polygonSize.width + 'px';
-                elt.style.height = polygonSize.height + 'px';
-            }
-//            this.fPolygon.setAttribute('transform', super.getTranslate());
-            this.updateObjectSize (obj);      
-	    }
-        this.fPolygon.style.fill = obj.fColor.getRGBString();
-        this.updatePos(polygon); 
+        }
+     	super.updateView(obj);
     }        
-
-    // cette fonction permet de déterminer si le polygon est centré au milieu de la scene
-    // si ce n'est pas le cas, on détermine son cadran pour le translater correctement  
-    getOrigin (polygon: SVGRect): boolean {
-        let xOrigin = Math.floor(this.scene2RelativeX(polygon.x + polygon.width/2)*100) /100;
-        let yOrigin = Math.floor(this.scene2RelativeY(polygon.y + polygon.height/2)*100) /100;
-        
-        if (xOrigin == 0 && yOrigin == 0) { return true; } // l'objet est centré        
-        else { return false; }
-    }
-     
 
 	updateObjectSize ( obj: IObject) : void {
         let elt = this.getHtml();
         let w = this.scene2RelativeWidth(elt.clientWidth);
         let h = this.scene2RelativeHeight(elt.clientHeight);
-        let x = this.scene2RelativeX(elt.clientLeft) + w/2;
-        let y = this.scene2RelativeY(elt.clientTop) + h/2;
 		obj.fPosition.setWidth (w);
 		obj.fPosition.setHeight (h);
-		obj.fPosition.setXPos (x);
-		obj.fPosition.setYPos (y);
-        console.log('w ' + elt.clientWidth + ' h ' + elt.clientHeight + ' x ' + elt.clientLeft + ' y ' + elt.clientTop)
-        console.log(x + ' ' + y)
     }	
-/*    	
-	getTransform (obj: IObject): string {
-		let hw = this.fPixWidth/2;
-		let hh = this.fPixHeight/2;
-		let dx = -(this.fPixWidth/2)  * (1 + obj.fPosition.getXOrigin());
-		let dy = -(this.fPixHeight/2) * (1 + obj.fPosition.getYOrigin());	
-        return super.getTransform(obj) + ` translate(${dx}px, ${dy}px)`;
-	}
-*/    
-	updateSvgPos (x: number, y: number): void { 
-    	let elt = this.getHtml();		
-		elt.style.left = x +"px";
-        elt.style.top  = y +"px";
-		this.fSVG.style.left = x +"px";
-        this.fSVG.style.top  = y +"px";
-	}    
 }
