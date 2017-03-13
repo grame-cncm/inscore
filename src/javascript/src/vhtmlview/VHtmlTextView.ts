1e8944ba16a2278887713ffa35c20aa125221603
D. Fober <fober@grame.fr>
2017-03-13 15:22:23 +0100

    text view cleaned: inherits html view with specific text style
    Former-commit-id: a08e50c4567663bb4d3ff0a799eea797c02cfa51

--- src/javascript/src/vhtmlview/VHtmlTextView.ts
+++ src/javascript/src/vhtmlview/VHtmlTextView.ts
@@ -10,32 +10,4 @@ class VHtmlTextView extends VHtmlHtmlView {
         elt.className = "inscore-txt";
         elt.style.verticalAlign = "middle";
 	}
-
-	static getPre( t: IText) {
-		if (t.fontModified()) return "<pre style='" + VHtmlTextView.getCSSFont(t)+"'>";
-		else return "<pre>";
-	}
-	
-	getText	( obj: IText) : string { return VHtmlTextView.getPre(<IText>obj) + obj.getText() +"</pre>"; }
-
-	setFont	( t: IText) : void {
-		let elt = this.getHtml();
-        elt.style.fontSize 		= t.getFontSize()+"px";
-        elt.style.fontFamily 	= t.getFontFamily();
-        elt.style.fontStyle 	= t.getFontStyle();
-        elt.style.fontWeight 	= VHtmlHtmlView.fontWeight2Num(t.getFontWeight()).toString();
-	}
-	static getCSSFont	( t: IText) : string {
-		let font = "font-size: " + t.getFontSize()+"px; "
-			     + "font-family: "  + t.getFontFamily() +"; "
-			     + "font-style: "   + t.getFontStyle() +"; "
-				 + "font-weight: "  + VHtmlHtmlView.fontWeight2Num(t.getFontWeight()) + "; ";
-
-		switch (t.fEffect.fEffectArray.length){
-			case 0 : return font;
-			case 1 : return font += "text-shadow: 0px 0px " + t.fEffect.getEffect()[0] + "px";
-			case 3 : return font += "text-shadow: " + VHtmlView.getEffects(t);
-		}
-		return font;
-	}
 }
