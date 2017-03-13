c7b4d39d20ac7e6ede7eff1c9af47aedc42b1db2
D. Fober <fober@grame.fr>
2017-03-13 15:19:40 +0100

    effects model redesigned
    Former-commit-id: 074af787051af6b0a68e840a3832db45eafa5564

--- src/javascript/src/model/IEffect.ts
+++ src/javascript/src/model/IEffect.ts
@@ -4,20 +4,17 @@
 
 class IEffect {
     fEffectModified: boolean;
-    fEffectName  : effect;
-    fEffectArray : Array<any>;
-    fShadow      : Array<number>;  //set shadow x and y
-    fShadowBlur   : number;
-    fBlur        : number;         //set blur
-    fColorize    : IColor;         //set color of shadow
-    fColorPerCent: number;         //set color perCent for colorize
+    fEffectType  : effect;
+    fEffectParams: Array<number>;
 
     private static fEffectStr2Num: { [id: string] : effect; } = {};
     private static fEffectNum2Str: { [id: number] : string; } = {};
+    private static fEffectParamsNum: Array<number> = [];		// expected parameters count
 
     constructor() {
         this.fEffectModified = false;
-        this.resetSetting();
+        this.fEffectType = effect.kNone;
+        this.fEffectParams= [];
         IEffect.buildEffect();
     }
 
@@ -31,6 +28,11 @@ class IEffect {
         IEffect.fEffectNum2Str[effect.kBlur]    = "blur";
         IEffect.fEffectNum2Str[effect.kColorize]= "colorize";
         IEffect.fEffectNum2Str[effect.kShadow]  = "shadow";
+
+        IEffect.fEffectParamsNum[effect.kNone]    = 0;
+        IEffect.fEffectParamsNum[effect.kBlur]    = 1;
+        IEffect.fEffectParamsNum[effect.kColorize]= 4;
+        IEffect.fEffectParamsNum[effect.kShadow]  = 7;
     }
 
     static effectStr2Num(str: string): { correct: boolean, val: number } {
@@ -44,63 +46,40 @@ class IEffect {
 
     // MODIFIED STATUS
 //--------------------------------------------------------------
+    type(): effect 				{ return this.fEffectType; }
+    params(): Array<number> 	{ return this.fEffectParams; }
+    param(i: number): number 	{ return this.fEffectParams[i]; }
+
     cleanup(): void 			{ this.fEffectModified = false; }
     effectModified(): boolean 	{ return this.fEffectModified; }
     modify() : void 			{ this.fEffectModified = true; }
 
-    getEffect()  : Array<any> 		 {  return this.fEffectArray}
+    getEffect()  : Array<any> 		 { 
+    	let out = new Array<any>();
+	    out[0] = IEffect.fEffectNum2Str[this.fEffectType];
+    	return out.concat(this.fEffectParams); 
+    }
     _getEffect() : GetArrayMethod 	 { return () => this.getEffect(); }
 
-    assignParams(effectArray: Array<any>) {
-       switch (effectArray[1]) {
-           case IEffect.effectNum2Str(effect.kNone).val :
-               if(effectArray.length  == 2) {
-                   this.fEffectName   = effect.kNone;
-                   this.resetSetting();
-               }else ITLError.badParameter("effect", effectArray);
-               break;
-           case IEffect.effectNum2Str(effect.kBlur).val :
-               if(effectArray.length == 3) {
-                   this.fEffectName = effect.kBlur;
-                   this.fBlur = effectArray[2];
-                   this.fEffectArray = [this.fBlur];
-               }else ITLError.badParameter("effect", effectArray);
-               break;
-           case IEffect.effectNum2Str(effect.kColorize).val :
-               if(effectArray.length == 6) {
-                   this.fEffectName  = effect.kColorize;
-                   this.fColorPerCent= effectArray[2];
-                   this.fColorize    = new IColor([effectArray[3], effectArray[4], effectArray[5]]);
-                   this.fEffectArray = [this.fColorPerCent,this.fColorize];
-               }else ITLError.badParameter("effect", effectArray);
-               break;
-           case IEffect.effectNum2Str(effect.kShadow).val :
-               if(effectArray.length == 9) {
-                   this.fEffectName = effect.kShadow;
-                   this.fShadow = [effectArray[2], effectArray[3]];
-                   this.fColorize = new IColor([effectArray[4], effectArray[5], effectArray[6], effectArray[7]/255]);
-                   this.fShadowBlur = effectArray[8];
-                   this.fEffectArray = [this.fColorize,this.fShadow,this.fShadowBlur];
-               }else ITLError.badParameter("effect", effectArray);
-               break;
-           default: ITLError.badParameter("effect", effectArray);
-       }
+    isNumberArray(a: Array<any>): boolean 	{ 
+    	for (let i=0; i< a.length; i++)
+		  	if (typeof a[i] != "number") return false;
+    	return true; 
+    }
+    
+    assignParams (type: number, params: Array<any>) : msgStatus {
+		if (! this.isNumberArray (params)) 						return msgStatus.kBadParameters;
+		if (params.length != IEffect.fEffectParamsNum[type])  	return msgStatus.kBadParameters;
+		this.fEffectType   = type;
+		this.fEffectParams = params;
+		this.modify();
+		return msgStatus.kProcessed;
     }
 
-    resetSetting(){
-        this.fEffectName = effect.kNone;
-        this.fEffectArray= [];
-        this.fShadow     = [0, 0];
-        this.fShadowBlur = 0;
-        this.fBlur       = 0;
-        this.fColorPerCent= 0;
-        this.fColorize   = new IColor([0, 0, 0, 1]);
-    }
-
-    setEffect (effect : Array<any>): msgStatus 	{
-        let style = IEffect.effectStr2Num(effect[1]);
-        if (!style.correct) { ITLError.badParameter("effect", effect);}
-        else this.modify(); this.assignParams(effect); return msgStatus.kProcessed;
+    setEffect (params : Array<any>): msgStatus 	{
+        let style = IEffect.effectStr2Num (params[1]);
+        if (!style.correct) return msgStatus.kBadParameters;
+        else return this.assignParams(style.val, params.slice(2));
     }
     _setEffect(): SetAnyArrayMethod { return (effect : Array<any>) => this.setEffect(effect) }
 }
