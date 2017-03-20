0c30476d0c0c65c2bcce404aefd2baed58262e47
Bardotti <kevin.bardotti@gmail.com>
2017-03-20 09:58:03 +0100

    div container replace svg container, work in IE and FF but in chrome
    Former-commit-id: b6c44cb5ac30daa906eced47d59e05854594a947

--- src/javascript/src/vhtmlview/VHtmlSvg.ts
+++ src/javascript/src/vhtmlview/VHtmlSvg.ts
@@ -16,14 +16,20 @@ interface SVGShape {
 }
 
 abstract class VHtmlSvg extends VHtmlView {
-    protected fSVG:SVGSVGElement;
-   
+   // protected fSVG:SVGSVGElement;
+       protected fSVG:HTMLElement;
+
     constructor(parent: VHtmlView) {
     	super (document.createElement('div'), parent);
-        this.fSVG = document.createElementNS('http://www.w3.org/2000/svg','svg');
+//this works on IE and FireFox
+		this.fSVG = document.createElement('div');
+
+//this works on chrome
+ /*       this.fSVG = document.createElementNS('http://www.w3.org/2000/svg','svg');
         this.fSVG.setAttribute('xmlns', "http://www.w3.org/2000/svg");
         this.fSVG.setAttribute('xmlns:xlink', "http://www.w3.org/1999/xlink");
-        this.fSVG.setAttribute('version', "1.1");
+        this.fSVG.setAttribute('version', "1.1");*/
+
     	this.getHtml().appendChild(this.fSVG);
 	}
 
