e5fbd86c04bca510607192213e8669569dc979df
D. Fober <fober@grame.fr>
2017-03-20 12:25:08 +0100

    store client width and height in a cache, it avoids endless growing size on updateview
    Former-commit-id: cebd620ca46a7609c6780f5213df33fb48835a05

--- src/javascript/src/vhtmlview/VHtmlGMNView.ts
+++ src/javascript/src/vhtmlview/VHtmlGMNView.ts
@@ -7,6 +7,8 @@ interface RefreshMethod 	{ (): void; }
 
 class VHtmlGMNView extends VHtmlSvg {
 	static fGMNScale = 2.3;		// scaling applied to get homogeneous size with inscore app
+	protected fClientWidth = 0;
+	protected fClientHeight = 0;
     
     constructor(parent: VHtmlView) {
         super(parent); 
@@ -15,18 +17,27 @@ class VHtmlGMNView extends VHtmlSvg {
     getSVGTarget() : SVGShape  { return this.fSVG; }
 
 	getViewScale (obj: IObject): number {
-		// this is to ensure a size similar to the native app version
-		return super.getViewScale(obj) * VHtmlGMNView.fGMNScale; 
+		// apply scaling to get a size similar to native application
+		return super.getViewScale(obj) * VHtmlGMNView.fGMNScale;
+	}
+
+	getClientSize (obj: IObject):  {w: number, h: number } {
+//		let cw = this.fSVG.clientWidth;		// works on Chrome but not on Firefox
+//		let ch = this.fSVG.clientHeight;
+		if (!this.fClientWidth) {
+        	let bbox = this.fSVG.getBoundingClientRect();
+        	this.fClientWidth = bbox.right - bbox.left;
+        	this.fClientHeight = bbox.bottom - bbox.top;
+        }
+		return  { w: this.fClientWidth, h: this.fClientHeight };
 	}
 
 	getSize (obj: IObject):  {w: number, h: number } {
-        let bbox = this.fSVG.getBoundingClientRect();
-		var cw = bbox.right - bbox.left;
-		var ch = bbox.bottom - bbox.top;
-
+        let size = this.getClientSize (obj);
 		let strokeWidth = obj.fPenControl.getPenWidth();
-        let w = cw + strokeWidth;
-        let h = ch + strokeWidth;
+        let w = size.w + strokeWidth;
+        let h = size.h + strokeWidth;
+        let scale = obj.fPosition.getScale();
 		return { w: (w ? w : 1), h: (h ? h : 1) };
 	}
       
@@ -72,14 +83,11 @@ class VHtmlGMNView extends VHtmlSvg {
 	_updateView	( obj: IObject) : RefreshMethod { return () => this.updateView (obj); }
 
 	updateObjectSize ( obj: IObject) : void {
-//		let cw = this.fSVG.clientWidth;
-//		let ch = this.fSVG.clientHeight;
+		if (obj.fPosition.getWidth()) return;
 
-        let bbox = this.fSVG.getBoundingClientRect();
-		var cw = bbox.right - bbox.left;
-		var ch = bbox.bottom - bbox.top;
-        let w = this.scene2RelativeWidth (cw);
-        let h = this.scene2RelativeHeight(ch);
+        let size = this.getClientSize (obj);
+        let w = this.scene2RelativeWidth (size.w);
+        let h = this.scene2RelativeHeight(size.h);
 		obj.fPosition.setWidth (w);
 		obj.fPosition.setHeight (h);
 		if (!w || !h)  setTimeout (this._updateView(obj), 50) ;		
