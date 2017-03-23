3a31afd1aef0cab17c284e05e88ba64700513f93
Bardotti <kevin.bardotti@gmail.com>
2017-03-23 10:08:09 +0100

    gmnView extends svgView common method are move to svgView, add SVGCode Method to know type of obj and updated view
    Former-commit-id: 0022b6885819dc0b85289e8c8f0e001c471882bd

--- src/javascript/src/vhtmlview/VHtmlGMNView.ts
+++ src/javascript/src/vhtmlview/VHtmlGMNView.ts
@@ -1,101 +1,50 @@
 
-///<reference path="VHtmlView.ts"/>
+///<reference path="VHtmlSvgView.ts"/>
 ///<reference path="../model/IGuidoCode.ts"/>
 ///<reference path="../externals/libGUIDOEngine.d.ts"/>
 
-interface RefreshMethod 	{ (): void; }
+interface RefreshMethod { (): void; }
 
-class VHtmlGMNView extends VHtmlView {
+class VHtmlGMNView extends VHtmlSvgView {
 	static fGMNScale = 2.3;		// scaling applied to get homogeneous size with inscore app
-	protected fClientWidth = 0;
-	protected fClientHeight = 0;
-    
-    constructor(parent: VHtmlView) {
-        super(document.createElement('div'), parent); 
-        this.getHtml().className = "inscore-gmn";
-    }
 
-	getViewScale (obj: IObject): number {
+	constructor(parent: VHtmlView) {
+		super(parent);
+		this.getHtml().className = "inscore-gmn";
+	}
+
+	getViewScale(obj: IObject): number {
 		// apply scaling to get a size similar to native application
 		return super.getViewScale(obj) * VHtmlGMNView.fGMNScale;
 	}
 
-	getClientSize (obj: IObject):  {w: number, h: number } {
-		if (!this.fClientWidth) {
-	        let svg = this.getsvg(this.getHtml());
-	        let box = svg.viewBox.baseVal;
-        	this.fClientWidth = box.width;
-        	this.fClientHeight = box.height;
-        }
-		return  { w: this.fClientWidth, h: this.fClientHeight };
+	getSVGCode(obj: IObject): string {
+		let gmn = <IGuidoCode>obj;
+		return gmn.getSVG();
 	}
 
-
-	getSize (obj: IObject):  {w: number, h: number } {
-        let size = this.getClientSize (obj);
-		let strokeWidth = obj.fPenControl.getPenWidth();
-        let w = size.w + strokeWidth;
-        let h = size.h + strokeWidth;
-		return { w: (w ? w : 1), h: (h ? h : 1) };
-	}
-      
-    updateView	(obj: IObject) : void {           	
-        // si le code gmn a changé, on le charge
-		if (obj.isNewData()) {
-            let gmn = <IGuidoCode>obj;
-            this.getHtml().innerHTML = gmn.getSVG();    
-        }   
-    	this.updateObjectSize (obj);     
-    	super.updateView(obj);
-	}
-	
-	getsvg (root: Node): SVGSVGElement {
-		let g : Node;
-	    let childs = root.childNodes;
-		for (let i = 0; i < childs.length && !g; i++) {
-			if (childs[i].nodeName == 'svg') {
-				g = childs[i];
-				break;
-			}
-        }
-        return <SVGSVGElement>g;
-	}
-	
-	getFirstSVGGroup (root: Node): SVGSVGElement {
-		let g : Node;
-	    let childs = root.childNodes;
+	getFirstSVGGroup(root: Node): SVGSVGElement {
+		let g: Node;
+		let childs = root.childNodes;
 		for (let i = 0; i < childs.length && !g; i++) {
 			if (childs[i].nodeName == 'g') {
 				g = childs[i];
 				break;
 			}
-			g = this.getFirstSVGGroup (childs[i]);
-        }
-        return <SVGSVGElement>g;
+			g = this.getFirstSVGGroup(childs[i]);
+		}
+		return <SVGSVGElement>g;
 	}
-	
-	updateColor (obj: IObject): void {
+
+	updateColor(obj: IObject): void {
 		let color = obj.fColor.getRGBString();
 		let alpha = obj.fColor.getSVGA();
-		let g = this.getFirstSVGGroup (this.getHtml());
+		let g = this.getFirstSVGGroup(this.getHtml());
 		if (g) {
-			g.style.stroke = color; 
-			g.style.strokeOpacity = alpha.toString(); 
-			g.style.fill = color; 
-			g.style.fillOpacity = alpha.toString(); 
-        }
-    }
-
-	_updateView	( obj: IObject) : RefreshMethod { return () => this.updateView (obj); }
-
-	updateObjectSize ( obj: IObject) : void {
-		if (obj.fPosition.getWidth()) return;
-
-        let size = this.getClientSize (obj);
-        let w = this.scene2RelativeWidth (size.w);
-        let h = this.scene2RelativeHeight(size.h);
-		obj.fPosition.setWidth (w);
-		obj.fPosition.setHeight (h);
-		if (!w || !h)  setTimeout (this._updateView(obj), 50) ;		
+			g.style.stroke = color;
+			g.style.strokeOpacity = alpha.toString();
+			g.style.fill = color;
+			g.style.fillOpacity = alpha.toString();
+		}
 	}
 }
--- src/javascript/src/vhtmlview/VHtmlSvgView.ts
+++ src/javascript/src/vhtmlview/VHtmlSvgView.ts
@@ -4,24 +4,67 @@
 
 class VHtmlSvgView extends VHtmlView {
 
+    protected fClientWidth = 0;
+    protected fClientHeight = 0;
+
     constructor(parent: VHtmlView) {
-        super(document.createElement('div'), parent); 
-    	this.getHtml().className = "inscore-svg";
-    } 
-    
-    updateView	(obj: IObject) : void {           	
+        super(document.createElement('div'), parent);
+        this.getHtml().className = "inscore-svg";
+    }
+
+    updateView(obj: IObject): void {
         if (obj.isNewData()) {
-            let svg = <ISVG>obj;
-            this.getHtml().innerHTML = svg.getSvg();
+            this.getHtml().innerHTML = this.getSVGCode(obj);;
             this.updateObjectSize(obj);
         }
-    	super.updateView(obj);
-	}   
+        super.updateView(obj);
+    }
 
-	getSize (obj: IObject):  {w: number, h: number } {
-		let strokeWidth = obj.fPenControl.getPenWidth();
-        let w = this.getHtml().clientWidth + strokeWidth;
-        let h = this.getHtml().clientHeight + strokeWidth;
-		return { w: (w ? w : 1), h: (h ? h : 1) };
-	}
+    getSVGCode(obj: IObject): string {
+        let svg = <ISVG>obj;
+        return svg.getSvg();
+    }
+
+    getsvg(root: Node): SVGSVGElement {
+        let g: Node;
+        let childs = root.childNodes;
+        for (let i = 0; i < childs.length && !g; i++) {
+            if (childs[i].nodeName == 'svg') {
+                g = childs[i];
+                break;
+            }
+        }
+        return <SVGSVGElement>g;
+    }
+
+    getClientSize(obj: IObject): { w: number, h: number } {
+        if (!this.fClientWidth) {
+            let svg = this.getsvg(this.getHtml());
+            let box = svg.viewBox.baseVal;
+            this.fClientWidth = box.width;
+            this.fClientHeight = box.height;
+        }
+        return { w: this.fClientWidth, h: this.fClientHeight };
+    }
+
+
+    getSize(obj: IObject): { w: number, h: number } {
+        let size = this.getClientSize(obj);
+        let strokeWidth = obj.fPenControl.getPenWidth();
+        let w = size.w + strokeWidth;
+        let h = size.h + strokeWidth;
+        return { w: (w ? w : 1), h: (h ? h : 1) };
+    }
+    _updateView(obj: IObject): RefreshMethod { return () => this.updateView(obj); }
+
+    updateObjectSize(obj: IObject): void {
+        if (obj.fPosition.getWidth()) return;
+
+        let size = this.getClientSize(obj);
+        let w = this.scene2RelativeWidth(size.w);
+        let h = this.scene2RelativeHeight(size.h);
+        obj.fPosition.setWidth(w);
+        obj.fPosition.setHeight(h);
+        if (!w || !h) setTimeout(this._updateView(obj), 50);
+    }
 }
