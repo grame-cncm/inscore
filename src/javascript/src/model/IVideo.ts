b9bc53ddcb7d8a2531f439c96958d948765310d8
Bardotti <kevin.bardotti@gmail.com>
2017-03-30 14:00:03 +0200

    video properties update
    Former-commit-id: 31a8e5fff6d6627a976058339e487490d243e5c2

--- src/javascript/src/model/IVideo.ts
+++ src/javascript/src/model/IVideo.ts
@@ -3,11 +3,36 @@
 class IVideo extends IObject {
     
     protected fFile: string;
+    fPlay: number;
+    fRate: number;
+    fVolume: number;
+    fVDate: number;
+    fModified: boolean;
 
     constructor(name: string, parent: IObject) {
         super(name, parent);
-        this.fTypeString = kVideoType;      
+        this.fTypeString = kVideoType;   
+        this.fPlay = 0;
+        this.fRate = 1;
+        this.fVolume = 1;
+        this.fVDate = 0;   
+
+        this.fMsgHandlerMap[kplay_GetSetMethod]          = new TMsgHandlerNum((n: number): void => { this.setPlay(n)  });
+        this.fMsgHandlerMap[krate_GetSetMethod]          = new TMsgHandlerNum((n: number): void => { this.setRate(n)  });
+        this.fMsgHandlerMap[kvolume_GetSetMethod]        = new TMsgHandlerNum((n: number): void => { this.setVolume(n)});
+        this.fMsgHandlerMap[kvdate_GetSetMethod]         = new TMsgHandlerNum((n: number): void => { this.setVDate(n) });
+
+        this.fGetMsgHandlerMap[kplay_GetSetMethod]       = new TGetMsgHandlerNum( () : number => { return this.fPlay  });
+        this.fGetMsgHandlerMap[krate_GetSetMethod]       = new TGetMsgHandlerNum( () : number => { return this.fRate  });
+        this.fGetMsgHandlerMap[kvolume_GetSetMethod]     = new TGetMsgHandlerNum( () : number => { return this.fVolume});
+        this.fGetMsgHandlerMap[kvdate_GetSetMethod]      = new TGetMsgHandlerNum( () : number => { return this.fVDate });
     }    
+
+    // MODIFIED STATUS
+    //--------------------------------------------------------------      
+   	cleanup() : void 	{ this.fModified = false; }
+   	modified(): boolean { return this.fModified; }   
+   	modify()  : void 	{ this.fModified = true; }
 
     colorAble(): void { }		// color attributes are not available for vidéos
     getFile(): string { return this.fFile; }
@@ -28,7 +53,17 @@ class IVideo extends IObject {
     }
 
     getSet(): IMessage	{
-    	return new IMessage(this.getOSCAddress(), [kset_SetMethod, this.fTypeString, "'"+this.getFile()+"'"]); 
+    	return new IMessage(this.getOSCAddress(), [kset_SetMethod, this.fTypeString, "'" + this.getFile() + "'"]); 
     }    
+
+    getPlay()     : number             { return this.fPlay  }
+    getVolume()   : number             { return this.fVolume}    
+    getRate()     : number             { return this.fRate  }
+    getVDate()    : number             { return this.fVDate }
+
+    setPlay  (play   : number ): void  { this.fPlay   = play;   this.modify()}
+    setVolume(volume : number ): void  { this.fVolume = volume; this.modify()}
+    setRate  (rate   : number ): void  { this.fRate   = rate;   this.modify()}
+    setVDate (vDate  : number ): void  { this.fVDate  = vDate;  this.modify()}
 }
 
--- src/javascript/src/vhtmlview/VHtmlVideoView.ts
+++ src/javascript/src/vhtmlview/VHtmlVideoView.ts
@@ -5,19 +5,25 @@
 
 class VHtmlVideoView extends VHtmlAutoSize {
     fVideo: HTMLVideoElement;
+	fFile : string;
     
     constructor(parent: VHtmlView) {
 		let video = document.createElement('video');
         super(video, parent); 
         this.fVideo = video;
+		this.fFile = "";
         this.getHtml().className = "inscore-video";
     }     
     
 	updateView	( obj: IObject) : void {
 		let video = <IVideo>obj;
-        this.fVideo.src  = video.getFile();
+        if (this.fFile != video.getFile() || this.fFile == "") {
+			this.fVideo.src = video.getFile();
+			this.fFile = video.getFile();
+		}
         super.updateView(obj);
-	}  
+		this.updateVideoControl(video);
+	}
  
 	setShadow (params: Array<number>) : void {
 		let color = new IColor( params.slice(2,6) );
@@ -25,4 +31,13 @@ class VHtmlVideoView extends VHtmlAutoSize {
 	}
    
 	getAutoElement() : HTMLElement 	{ return this.fVideo; }
+
+	updateVideoControl(video: IVideo) : void {
+		if (video.modified()){
+			video.fPlay ? this.fVideo.play() : this.fVideo.pause();
+			this.fVideo.volume = video.fVolume;
+			this.fVideo.playbackRate = video.fRate;
+			this.fVideo.currentTime += video.fVDate;
+		}
+	}
 }
