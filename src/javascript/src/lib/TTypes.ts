88da6b67c062ec2081e773a05c57ab1bf48fb8be
D. Fober <fober@grame.fr>
2017-03-31 11:11:00 +0200

    new TPair type
    Former-commit-id: da8a352f3f417f262809e8a14e8ae9e18af0a883

--- src/javascript/src/lib/TTypes.ts
+++ src/javascript/src/lib/TTypes.ts
@@ -2,3 +2,4 @@
 type TMouseEnv 	=	{ x: number, y: number, ax: number, ay: number, sx: number, sy: number };
 type TPosition 	=	{ x: number, y: number };
 type TSize 		=	{ w: number, h: number };
+type TPair<T> 	=	{ first: T, second: T };
