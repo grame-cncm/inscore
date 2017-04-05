cd3bcf74f2489932c1c085ace2b6a57a3cf2bd6d
Bardotti <kevin.bardotti@gmail.com>
2017-04-05 17:58:07 +0200

    scene color is now on background color css attribute
    Former-commit-id: 2a8b13ad0cd3c456aa2024eb70f706510df4be7e

--- src/javascript/src/vhtmlview/VHtmlSceneView.ts
+++ src/javascript/src/vhtmlview/VHtmlSceneView.ts
@@ -8,20 +8,20 @@
 // VHtmlSceneContainer detects the current element 
 // and provides it as a container to the scene
 //--------------------------------------------------
-class VHtmlSceneContainer extends VHtmlView {  
+class VHtmlSceneContainer extends VHtmlView {
 
-	static getHtml() : HTMLElement {
+	static getHtml(): HTMLElement {
 		let scripts = document.getElementsByTagName('script');
-    	return <HTMLElement>(scripts[scripts.length - 1].parentNode);
+		return <HTMLElement>(scripts[scripts.length - 1].parentNode);
 	}
 
-    constructor() 	{ super (VHtmlSceneContainer.getHtml()); }   
-	setDefaultPositionStyle() : void 				{ /* do not change position style */ }
+	constructor() { super(VHtmlSceneContainer.getHtml()); }
+	setDefaultPositionStyle(): void { /* do not change position style */ }
 
-	updatePos() : void {
-    	let size = TWindow.getSize();
-        let w = Math.min(size.w, size.h);
-        this.setPos ((size.h - w) / 2.0, (size.w - w) / 2.0, w, w);
+	updatePos(): void {
+		let size = TWindow.getSize();
+		let w = Math.min(size.w, size.h);
+		this.setPos((size.h - w) / 2.0, (size.w - w) / 2.0, w, w);
 	}
 }
 
@@ -30,91 +30,103 @@ class VHtmlSceneView extends VHtmlView {
 	protected fAbsolutePos: boolean;
 	protected fReferenceWidth: number;
 
-    //-----------name for document getElementById-------------
-    protected fName : string
+	//-----------name for document getElementById-------------
+	protected fName: string
 
-    constructor(name: string) {
-    	let parent = new VHtmlSceneContainer();
-    	super (document.createElement('div'), parent);
-    	this.fDoc = parent;
-    	this.fReferenceWidth = Math.min(screen.width, screen.height);
+	constructor(name: string) {
+		let parent = new VHtmlSceneContainer();
+		super(document.createElement('div'), parent);
+		this.fDoc = parent;
+		this.fReferenceWidth = Math.min(screen.width, screen.height);
 
-        let div = this.getHtml();
+		let div = this.getHtml();
 		div.className = "inscore-scene";
-		div.setAttribute("name", name); 
+		div.setAttribute("name", name);
 		div.addEventListener("drop", inscore_dropEvent, false);
-		div.addEventListener("dragover", inscore_dragOverEvent, false);	
+		div.addEventListener("dragover", inscore_dragOverEvent, false);
 		this.fAbsolutePos = false;
 
-//		let style = window.getComputedStyle(div);
-//	    this.fAbsolutePos = (style.position === 'absolute');
-    }
+		//		let style = window.getComputedStyle(div);
+		//	    this.fAbsolutePos = (style.position === 'absolute');
+	}
 
-	setDefaultPositionStyle() : void 				{ /* none: default is relative */ }
-	getViewScale (obj: IObject): number 			{ return Math.min(obj.fPosition.getWidth(), obj.fPosition.getHeight()) * obj.fPosition.getScale(); }
+	setDefaultPositionStyle(): void { /* none: default is relative */ }
+	getViewScale(obj: IObject): number { return Math.min(obj.fPosition.getWidth(), obj.fPosition.getHeight()) * obj.fPosition.getScale(); }
 
-	relative2SceneX(x: number) : number 			{ return this.fParent.fLeft + super.relative2SceneX(x); }
-	relative2SceneY(y: number) : number 			{ return this.fParent.fTop + super.relative2SceneY(y); }
-	relative2SceneWidth(width: number) : number 	{ return this.fReferenceWidth * width / 2; }
-	relative2SceneHeight(height: number) : number	{ return this.fReferenceWidth * height / 2; }
+	relative2SceneX(x: number): number { return this.fParent.fLeft + super.relative2SceneX(x); }
+	relative2SceneY(y: number): number { return this.fParent.fTop + super.relative2SceneY(y); }
+	relative2SceneWidth(width: number): number { return this.fReferenceWidth * width / 2; }
+	relative2SceneHeight(height: number): number { return this.fReferenceWidth * height / 2; }
 
-	getTranslate (obj: IObject): string 			{ return this.fAbsolutePos ? super.getTranslate(obj) : ""; }
-	scenePosition(obj: IObject): void{
+	getTranslate(obj: IObject): string { return this.fAbsolutePos ? super.getTranslate(obj) : ""; }
+	scenePosition(obj: IObject): void {
 		let scene = <IScene>obj;
 		this.fAbsolutePos = scene.fAbsolutePos;
 		if (this.fAbsolutePos) this.getHtml().style.position = IScene.kAbsolute;
 	}
 
-	updateView	( obj: IObject) : void {
+	updateColor(obj: IObject): void {
+		if (obj.fColor.modified()) {
+			let elt = this.colorTarget();
+			elt.style.backgroundColor = obj.fColor.getRGBString();
+			let alpha = obj.fColor.getA() / 255;
+			elt.style.opacity = alpha.toString();
+		}
+	}
+
+	updateView(obj: IObject): void {
 		this.scenePosition(obj);
 		this.fDoc.updatePos();			// not necessary
 		this.fWidth = this.fHeight = this.fDoc.fWidth;
 		super.updateView(obj);
 		if (!this.fAbsolutePos) {
 			let elt = this.getHtml();
-			elt.style.left   = "";
-        	elt.style.top 	 = "";
+			elt.style.left = "";
+			elt.style.top = "";
 		}
-        this.enableDisableFullScreen(obj);
+		//   this.enableDisableFullScreen(obj);
 	}
 
-	   //-------------------FullScreenManagement---------------------
-    enableDisableFullScreen(obj: IObject) {
-		let scene = <IScene>obj;
-        if (scene.fFullScreen) this.launchFullscreen(this.getHtml());
-        else this.exitFullscreen();
-    }
+	//-------------------FullScreenManagement---------------------
+	//don't work actually
+	/*
+ enableDisableFullScreen(obj: IObject) {
+	 let scene = <IScene>obj;
+	 if (scene.fFullScreen) this.launchFullscreen(this.getHtml());
+	 else this.exitFullscreen();
+ }
 
-    // Find the right method, call on correct element
-    launchFullscreen(element: HTMLElement): void {
-        if (element.requestFullscreen) {
-            //Standard
-            element.requestFullscreen();
-        } else if (element.mozRequestFullScreen) {
-            //Mozilla
-            element.mozRequestFullScreen();
-        } else if (element.webkitRequestFullscreen) {
-            //Chrome - Edge
-            element.webkitRequestFullscreen();
-        } else if (element.msRequestFullscreen) {
-            //IE
-            element.msRequestFullscreen();
-        }
-    }
+ // Find the right method, call on correct element
+ launchFullscreen(element: HTMLElement): void {
+	 if (element.requestFullscreen) {
+		 //Standard
+		 element.requestFullscreen();
+	 } else if (element.mozRequestFullScreen) {
+		 //Mozilla
+		 element.mozRequestFullScreen();
+	 } else if (element.webkitRequestFullscreen) {
+		 //Chrome - Edge
+		 element.webkitRequestFullscreen();
+	 } else if (element.msRequestFullscreen) {
+		 //IE
+		 element.msRequestFullscreen();
+	 }
+ }
 
-    exitFullscreen(): void {
-        if (document.exitFullscreen) {
-            //Standard
-            document.exitFullscreen();
-        } else if (document.mozCancelFullScreen) {
-            //Mozilla
-            document.mozCancelFullScreen();
-        } else if (document.webkitExitFullscreen) {
-            //Chrome - Edge
-            document.webkitExitFullscreen();
-        } else if (document.msExitFullscreen) {
-            //IE
-            document.msExitFullscreen();
-        }
-    }
+ exitFullscreen(): void {
+	 if (document.exitFullscreen) {
+		 //Standard
+		 document.exitFullscreen();
+	 } else if (document.mozCancelFullScreen) {
+		 //Mozilla
+		 document.mozCancelFullScreen();
+	 } else if (document.webkitExitFullscreen) {
+		 //Chrome - Edge
+		 document.webkitExitFullscreen();
+	 } else if (document.msExitFullscreen) {
+		 //IE
+		 document.msExitFullscreen();
+	 }
+ }
+ */
 }
