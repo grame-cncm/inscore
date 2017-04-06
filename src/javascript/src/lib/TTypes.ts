b30f0fb4d453637bb92adcfd2bfa8b013f4bb393
D. Fober <fober@grame.fr>
2017-04-06 15:12:56 +0200

    TMouseEnv redefined using TPosition
    Former-commit-id: e4b1a397d654dd14d46bdb4b40d4c62fd97924b9

--- src/javascript/src/lib/TTypes.ts
+++ src/javascript/src/lib/TTypes.ts
@@ -1,5 +1,5 @@
 
-type TMouseEnv 	=	{ x: number, y: number, ax: number, ay: number, sx: number, sy: number };
 type TPosition 	=	{ x: number, y: number };
+type TMouseEnv 	=	{ rel: TPosition, abs: TPosition, parent: TPosition };
 type TSize 		=	{ w: number, h: number };
 type TPair<T> 	=	{ first: T, second: T };
