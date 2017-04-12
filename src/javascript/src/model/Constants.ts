e652ee93c97c9dcca335bc26598c75014243d2eb
D. Fober <fober@grame.fr>
2017-04-12 11:11:32 +0200

    - default rate is now 10 - object size declaration
    Former-commit-id: 8b60f9a34a4980faac0bedbdfdaf48c01bf0a27f

--- src/javascript/src/model/Constants.ts
+++ src/javascript/src/model/Constants.ts
@@ -26,4 +26,5 @@ const kApplType             : string	= "IAppl";
 // inscore type for inscore scripts
 const kInscoreFile  : string	= "inscore";
 
-var kDefaultRate:			 number = 50;	// the default inscore time task rate in ms
+var kDefaultRate:			 number = 10;	// the default inscore time task rate in ms
+var kObjectSize:			 number = 2;	// an object size in internal representation
