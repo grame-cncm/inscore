b6db0e71ca930b43dd8de69cc3a6470ea9320252
D. Fober <fober@grame.fr>
2017-04-28 14:35:49 +0200

    new generic setFile method
    Former-commit-id: 17bf3659abb8d2a335375327a233b644bd0f370d

--- src/javascript/src/lib/Tools.ts
+++ src/javascript/src/lib/Tools.ts
@@ -19,12 +19,12 @@ class Tools {
     }
 
     // Read file content
-    // Actually error doesn't catch anything
+    // Actually error does not catch anything
     static readFile(src: string, success: TReadHandler, error: TReadHandler): void {
         let content: string = "";
         let receiver = document.createElement("iframe");
         receiver.style.display = "none";
-        receiver.onload = () => {
+        receiver.onload = () => { 
             let doc = receiver.contentWindow || receiver.contentDocument;
             if (doc.document) doc = doc.document;
             content = doc.getElementsByTagName("body")[0].innerText;
@@ -35,4 +35,27 @@ class Tools {
         receiver.src = src;
         document.body.appendChild (receiver);
     }
+
+    // Handling a set file message
+    static setFile(msg: IMessage, success: TReadHandler, error: TReadHandler): string { 
+        let n = msg.size();
+        if (n == 3) {
+        	let file = msg.paramStr(2);
+        	if (file.correct) {
+                Tools.readFile(file.value, success, error);
+	            return file.value;
+        	}
+        }
+        return null;
+    }
+
+	static string2svg (str: string) : string {
+		let n = str.length+1;
+		return '<?xml version="1.0"?>' +
+				'<svg width="' + (n*10) + '" height="20"' +
+				' xmlns="http://www.w3.org/2000/svg"  version="1.1">' +
+				'<g font-family="courier" font-size="10" >' +
+				'<text x="10" y="10">' +
+				str +'</g></svg>';
+	}
 }
