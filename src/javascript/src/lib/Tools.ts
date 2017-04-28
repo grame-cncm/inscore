39c50ecd3cdd5ed0bfa600bef08f9510fe1129c9
D. Fober <fober@grame.fr>
2017-04-28 18:32:02 +0200

    error detection on readfile
    Former-commit-id: 2c60ef8f15301ebe87acc49b97b3999c7c99897e

--- src/javascript/src/lib/Tools.ts
+++ src/javascript/src/lib/Tools.ts
@@ -24,13 +24,17 @@ class Tools {
         let content: string = "";
         let receiver = document.createElement("iframe");
         receiver.style.display = "none";
+        receiver.onerror = function() { console.log ('error: failed to load ' + src) } ;
+        receiver.onabort = function() { console.log ('abort: failed to load ' + src) } ;
         receiver.onload = () => { 
             let doc = receiver.contentWindow || receiver.contentDocument;
+            let title = doc.document.title;
             if (doc.document) doc = doc.document;
             content = doc.getElementsByTagName("body")[0].innerText;
             receiver.remove();
-            if (content !== "") success(content);
-            else error(content);
+            if ((title === "404 Not Found") || (content.indexOf ("Not Found") == 0))
+            	error(content);
+            else success(content);
         }
         receiver.src = src;
         document.body.appendChild (receiver);
@@ -50,12 +54,18 @@ class Tools {
     }
 
 	static string2svg (str: string) : string {
-		let n = str.length+1;
-		return '<?xml version="1.0"?>' +
-				'<svg width="' + (n*10) + '" height="20"' +
-				' xmlns="http://www.w3.org/2000/svg"  version="1.1">' +
-				'<g font-family="courier" font-size="10" >' +
-				'<text x="10" y="10">' +
-				str +'</g></svg>';
+		let n = str.length;
+		let header = '<svg  width="'+ n*6 + '" height="14" ';
+		return header + 'xmlns="http://www.w3.org/2000/svg"  version="1.1"> <g  font-family="courier" font-size="10"><text x="0" y="10">' + str +'</text></g></svg>';
+	}
+
+	static error2svg() : string  {
+		return '<svg width="68" height="68" xmlns="http://www.w3.org/2000/svg"><g>' +
+			'<rect rx="5" height="64" width="64" y="2" x="2" stroke-width="4" stroke="#000" fill="#e00"/>' +
+			'<rect rx="4" height="14" width="34" y="26" x="17" stroke-width="0" fill="#fff"/></g></svg>';
+
+//		return '<svg width="128" height="128" xmlns="http://www.w3.org/2000/svg"><g>' +
+			'<rect rx="5" height="124" width="124" y="2" x="2" stroke-width="4" stroke="#000" fill="#e00"/>' +
+			'<rect rx="5" height="24" width="64" y="52" x="32" stroke-width="0" fill="#fff"/></g></svg>';
 	}
 }
