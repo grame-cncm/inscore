9c6f7fd28515a458886a0adcb9162e4e49fd445a
Dominique Fober <fober@grame.fr>
2019-03-04 13:27:10 +0100

    packaging (windows)

--- package/MakePkg.bat
+++ package/MakePkg.bat
@@ -1,10 +1,22 @@
 
+@echo off
+
+set /p VERSION=<..\version.txt
+set PACK=INScore-%VERSION%-win64
+set BUILDDIR=..\build\bin
+
+echo Call windeployqt
+windeployqt %BUILDDIR%\INScoreViewer.exe
 
 
-windeployqt ..\build\bin\INScoreViewer.exe
+IF NOT EXIST build (
+	echo Creating build folder
+	mkdir build
+)
 
-mkdir build
 cd build
+echo Creating cmake setup
 cmake .. -G "Visual Studio 14 2015 Win64"
 cpack -g NSIS64
 move INScore-*.exe ..
+cd ..
