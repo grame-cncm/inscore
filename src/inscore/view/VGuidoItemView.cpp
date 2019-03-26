be775f0452db2c3e48acf97100549fd26624c5fe
Dominique Fober <fober@grame.fr>
2019-03-26 18:16:28 +0100

    time type changed

--- src/inscore/view/VGuidoItemView.cpp
+++ src/inscore/view/VGuidoItemView.cpp
@@ -105,7 +105,7 @@ class RolledUnrolledCollector: public TimeMapCollector
 //----------------------------------------------------------------------
 void QCachedGuidoGraphicsItem::paint( QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget)
 {
-	__is_uint64 time = getTime();
+	double time = getTime();
 	QRectF rect = boundingRect();
 	QSize size (rect.width(), rect.height());
 //qDebug() << "QCachedGuidoGraphicsItem::paint rect" << rect;
