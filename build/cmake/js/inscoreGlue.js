7eb7022778a2b6bd04663aa78cf0b0a8669d12f8
Dominique Fober <fober@grame.fr>
2020-03-05 14:32:14 +0100

    initialisation process

--- build/cmake/js/inscoreGlue.js
+++ build/cmake/js/inscoreGlue.js
@@ -5,17 +5,33 @@
 // INScore interface
 //----------------------------------------------------------------------------
 class INScoreGlue {
+
     constructor(inscore) {
-    	this.fInscore = inscore;
+    	this.fInscore = new INScore;
+    	this.fTimeTask = 0;
+    	this.fInscore.initialize ().then (() => { this.initialize(); });
     }
-    
+	
     //------------------------------------------------------------------------
     // initialization
     initialize () {
+		this.fInscore.start(0,0,0);
+		this.fTimeTask = setInterval( () => { this.fInscore.timeTask(); }, this.fInscore.getRate());
 		let divs = document.getElementsByClassName("inscore");
-		for (let i=0; i<divs.length; i++) {
-			this.allowdrop (divs[i]);
-		}
+		for (let i=0; i<divs.length; i++)
+			this.initDiv (divs[i], false);
+		divs = document.getElementsByClassName("inscore2");
+		for (let i=0; i<divs.length; i++)
+			this.initDiv (divs[i], true);
+	}
+    
+    //------------------------------------------------------------------------
+    // inscore div initialization
+	initDiv (div, v2) {
+		const scene = div.id;
+		if (!scene) scene = "scene";
+		this.fInscore.postMessageStr ("/ITL/"+scene, "new");	
+		this.allowdrop (div);
 	}
 
     //------------------------------------------------------------------------
@@ -28,6 +44,17 @@ class INScoreGlue {
 	}
 
     //------------------------------------------------------------------------
+    // load an inscore file
+	loadInscore(file, v2) {
+		let reader = new FileReader();				
+		reader.readAsText (file);
+		if (v2)
+			reader.onloadend = (event) => { this.fInscore.loadInscore2(reader.result)};
+		else
+			reader.onloadend = (event) => { this.fInscore.loadInscore(reader.result)};
+	}
+
+    //------------------------------------------------------------------------
     // files drop support
 	filedropped(e) {
 		let filelist = e.dataTransfer.files;
@@ -35,17 +62,13 @@ class INScoreGlue {
 	
 		let filecount = filelist.length;
 		for (let i = 0; i < filecount; i++ ) {
-			let file 	= filelist[i]
-			let fileName = filelist[i].name;
-			let properties = this.getFileProperties(fileName);
-			let name 	= properties.name;
-			let ext 	= properties.ext.toLocaleLowerCase();
-			if (ext == "inscore") {
-				let reader = new FileReader();				
-				reader.readAsText (file);
-				console.log ("file " + name + " dropped : ");
-				reader.onloadend = function(event) { console.log ("file content: " + reader.result); };						
-			}
+			const file 	= filelist[i]
+			const fileName = filelist[i].name;
+			const properties = this.getFileProperties(fileName);
+			const name 	= properties.name;
+			const ext 	= properties.ext.toLocaleLowerCase();
+			if (ext == "inscore") 		this.loadInscore (file, false);
+			else if (ext == "inscore2") this.loadInscore (file, true);
 			else {
 				console.log ("not an inscore file " + name);
 			}
@@ -53,20 +76,25 @@ class INScoreGlue {
 	}
 
 
-	drop( e) {
+	drop( e ) {
 		let data = e.dataTransfer.getData("Text");
-		if (data)
-			console.log ("text dropped : " + data);
+		if (data) {
+//   			console.log ("text dropped : " + data );
+ 			this.fInscore.loadInscore(data, false);
+		}
 		else this.filedropped (e);
-		e.target.style.border = "none";
+		e.target.style.border = e.target.getAttribute('savedborder');
 	}
 		
     //------------------------------------------------------------------------
     // activate drag & drop on inscore divs
 	allowdrop (div) {
-		div.addEventListener ("dragenter", (event) => { div.style.border = "1px solid grey"; }, false);
-		div.addEventListener ("dragleave", (event) => { div.style.border = "none"; }, false);
+		div.addEventListener ("dragenter", (event) => { div.setAttribute('savedborder', div.style.border);
+														div.style.border = "1px solid grey"; }, false);
+		div.addEventListener ("dragleave", (event) => { div.style.border = div.getAttribute('savedborder'); }, false);
 		div.addEventListener ("dragover",  (event) => { event.preventDefault(); }, false);
 		div.addEventListener ("drop",      (event) => { event.preventDefault(); this.drop ( event );} , false);
     }    
  }
+
+var gGlue = new INScoreGlue;
