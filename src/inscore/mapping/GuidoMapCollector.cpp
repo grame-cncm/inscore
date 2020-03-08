ec4f6e337001a73a4c5762f0e51cc0f6ee598149
D. Fober <fober@grame.fr>
2020-03-08 17:56:42 +0100

    commented code removed

--- src/inscore/mapping/GuidoMapCollector.cpp
+++ src/inscore/mapping/GuidoMapCollector.cpp
@@ -158,12 +158,6 @@ void GuidoSystemCollector::process (const ScoreLayoutInfos& score, Time2GraphicM
 //----------------------------------------------------------------------
 void GuidoSystemCollector::processNoDiv (const ScoreLayoutInfos& score, Time2GraphicMap* outmap)
 {
-	int M,m,s;
-// already checked at upper level
-//	GuidoGetVersionNums (&M, &m, &s);
-//	if (GuidoCheckVersionNums (1, 4, 2) != guidoNoErr)
-//		ITLErr << "correct system map requires GUIDOEngine version 1.4.2 or greater - current version is" << GuidoGetVersionStr() << ITLEndl;
-
 	GuidoMapCollector systemCollector (kGuidoSystem);
 	GuidoMapCollector slicesCollector (kGuidoSystemSlice);
 
