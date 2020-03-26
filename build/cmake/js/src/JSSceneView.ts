47a06d801e648681c15689a1c0c6c681ed4bf663
Dominique Fober <fober@grame.fr>
2020-03-26 08:06:58 +0100

    remove useless private field

--- build/cmake/js/src/JSSceneView.ts
+++ build/cmake/js/src/JSSceneView.ts
@@ -4,12 +4,9 @@
 //----------------------------------------------------------------------------
 class JSSceneView extends JSObjectView {
 
-	private firstUpdate: boolean;
-
     constructor(id: string, objid: number) {
     	super(document.getElementById(id), null, false);
 		this.updateObjectSize (objid);
-		this.firstUpdate = true;
     }
 	
 	updateColor(color: OColor) {
@@ -17,7 +14,6 @@ class JSSceneView extends JSObjectView {
 		div.style.background = color.rgb;
 		div.style.opacity 	 = color.alpha.toString();
 	}
-
 
 	toString() : string					{ return "JSSceneView"; }
 }
