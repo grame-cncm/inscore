99c0dcdb994ce260a006146dca2830d6fedc4bb0
Dominique Fober <fober@grame.fr>
2020-03-28 18:17:26 +0100

    setWidth and setHeight are now virtual

--- src/inscore/model/IPosition.h
+++ src/inscore/model/IPosition.h
@@ -130,9 +130,9 @@ class IPosition
 		virtual void	print(std::ostream& out) const;
 
 		/// \brief Sets the width
-		void			setWidth(float width)		{ fWidth = width; fModified = true; }
+		virtual void	setWidth(float width)		{ fWidth = width; fModified = true; }
 		/// \brief Sets the height
-		void			setHeight(float height)		{ fHeight = height; fModified = true; }
+		virtual void	setHeight(float height)		{ fHeight = height; fModified = true; }
 
 		/*!
 			\brief sets an object \c x position in its scene			
