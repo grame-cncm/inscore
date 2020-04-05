c2c1a9d6ee08ac36740f50cadde5521954bb62eb
Dominique Fober <fober@grame.fr>
2020-04-05 16:22:33 +0200

    use svg bbox to update size

--- javascript/src/JSCurveView.ts
+++ javascript/src/JSCurveView.ts
@@ -22,18 +22,11 @@ class JSCurveView extends JSSvgBase {
 		let n = curve.size();
 		for (let i=0; i < (n-7); i+=8) {
 			let ps = this.relative2SceneCurve (curve.get(i), curve.get(i+1), curve.get(i+2), curve.get(i+3), curve.get(i+4), curve.get(i+5), curve.get(i+6), curve.get(i+7)); 
-			// let attributes = 'M' + ps[0] + ',' + ps[1] + ' ' + 'C' + ps[2] + ',' + ps[3] + ' ' + ps[4] + ',' + ps[5] + ' ' + ps[6] + ',' + ps[7];
-			// let attributes = `M${ps[0]},${ps[1]} C${ps[2]},${ps[3]} ${ps[4]},${ps[5]} ${ps[6]},${ps[7]}`;
 			this.fCurve.setAttribute('d', `M${ps[0]},${ps[1]} C${ps[2]},${ps[3]} ${ps[4]},${ps[5]} ${ps[6]},${ps[7]}`);
-			let r = this.fCurve.getBoundingClientRect();
 
 			// update object size
-			let obj = INScore.objects().create(objid);
-			// let div = this.getElement();
-			this.updateObjectSizeSync (obj, r.width, r.height);
-			// obj.updateWidth  (this.scene2RelativeWidth  (r.width)); 
-			// obj.updateHeight (this.scene2RelativeHeight (r.height)); 
-			INScore.objects().del (obj);
+			let bb = this.fSVG.getBBox();
+-			this.updateObjectSizeSync (obj, bb.width, bb.height);
 		}
 		return true;
 	}
