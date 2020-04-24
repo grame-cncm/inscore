d9f207ed4f06ffbba52474ecce2b5f669c2dfa28
Dominique Fober <fober@grame.fr>
2020-04-24 19:31:27 +0200

    inscore class interface

---
+++ javascript/src/lib/inscore.d.ts
@@ -0,0 +1,46 @@
+
+///<reference path="libINScore.d.ts"/>
+
+//----------------------------------------------------------------------------
+// INScore interface
+//----------------------------------------------------------------------------
+interface INScore {
+
+    initialise():Promise<any> ;
+
+    //------------------------------------------------------------------------
+    // INScore interface
+    start() : void;
+	stop()  : void;
+
+	loadInscore	 (script: string, autoparse: boolean) : boolean;
+	loadInscore2 (script: string) : boolean;
+
+	postMessage	(adr: string, msg: TMessage)	: void;
+	postMessageStr (adr: string, meth: string)	: void;
+	postMessageStrI	(adr: string, meth: string, val: number) : void;
+	postMessageStrF	(adr: string, meth: string, val: number) : void;
+	postMessageStrStr(adr: string, meth: string, val: string): void;
+
+    delayMessage (adr: string, msg: TMessage)   : void;
+	newMessage ()                               : TMessage;
+	newMessageM (meth: string)                  : TMessage;
+	delMessage (msg: TMessage)                  : void;
+
+    msgAddStr (msg: TMessage, str: string)      : void;
+	msgAddF   (msg: TMessage, val: number)      : void;
+	msgAddI   (msg: TMessage, val: number)      : void;
+
+    version ()				: number; 
+	versionStr ()			: string; 
+	guidoversion ()			: string; 
+	musicxmlversion ()		: string; 
+        
+    //------------------------------------------------------------------------
+    // INScore glue interface
+    getRate() 		: number;
+	timeTask ()	    : void;
+	sorterTask()	: void;
+}
+
+declare var inscore : INScore;
