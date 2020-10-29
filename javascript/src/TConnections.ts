127c431f727714a5ee358674801b74b05f875e1e
Dominique Fober <fober@grame.fr>
2020-10-29 15:48:02 +0100

    try EventSource communication (has CORS issue)

--- javascript/src/TConnections.ts
+++ javascript/src/TConnections.ts
@@ -1,7 +1,6 @@
 
 ///<reference path="lib/libINScore.d.ts"/>
 
-interface wserror extends Event { message: string };
 
 class TConnection {
 	fSocket: WebSocket = null;
@@ -13,11 +12,13 @@ console.log ("TConnection create new connection to " + url);
 		// this.fUrl = url;
 		this.fEventSrc = new EventSource (url);
 		this.fEventSrc.onmessage = (event) => { this.processData (event.data) };
+		this.fEventSrc.onopen = () => { console.log("Connection to " + this.fUrl + " established"); };
+		this.fEventSrc.onerror = (error: Event) => { console.log (`[http error] ${error}`); };
 
-		this.fSocket = new WebSocket(url);
-		this.fSocket.onopen = () => { console.log("Connection to " + this.fUrl + " established"); };
-		this.fSocket.onmessage = (event) => { this.processData (event.data) };
-		this.fSocket.onerror = (error: wserror) => { console.log (`[websocket error] ${error}`); };
+		// this.fSocket = new WebSocket(url);
+		// this.fSocket.onopen = () => { console.log("Connection to " + this.fUrl + " established"); };
+		// this.fSocket.onmessage = (event) => { this.processData (event.data) };
+		// this.fSocket.onerror = (error: wserror) => { console.log (`[websocket error] ${error}`); };
 	}
 
 	close() { if (this.fSocket) this.fSocket.close(); this.fSocket =  null; }
