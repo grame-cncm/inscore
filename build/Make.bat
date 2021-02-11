f12513d165a51bbcc11f91b58e8cc54804b79a31
Dominique Fober <fober@grame.fr>
2021-02-11 18:14:56 +0100

    add install and package to build script

--- build/Make.bat
+++ build/Make.bat
@@ -2,15 +2,37 @@
 @echo off
 
 set MSVC="Visual Studio 15 2017 Win64"
+set VERSION="1.2.7"
 
 IF [%1]==[]     GOTO USAGE
 IF %1==modules 	CALL :MODULES
 IF %1==inscore 	CALL :COMPILE
 IF %1==deploy 	CALL :DEPLOY
+IF %1==install 	CALL :INSTALL
+IF %1==package 	CALL :PACKAGE
 IF %1==help 	GOTO USAGE
 
 GOTO DONE
 
+@rem --- installation -------------------------------
+:INSTALL
+cd inscoredir
+cmake .. -DPREFIX=../INScore-%VERSION%
+cmake --build . --config Release --target install
+cd ..
+EXIT /B
+
+@rem --- packaging ----------------------------------
+:PACKAGE
+cd inscoredir
+cmake .. -DPACK=on
+cmake --build . --config Release 
+cpack -G NSIS64
+move INScore-*.exe ..
+cd ..
+EXIT /B
+
+@rem --- deployment ----------------------------------
 :DEPLOY
 	echo Call windeployqt
 	set dest=bin
@@ -25,6 +47,7 @@ GOTO DONE
 	xcopy /y "..\modules\libmusicxml\build\lib\musicxml2.dll" "%dest%\Plugins\"
 EXIT /B
 
+@rem --- submodules compilation ----------------------
 :MODULES
 	echo Compiles submodules
 	cd ../modules
@@ -33,6 +56,7 @@ echo in modules
 	cd ../build
 EXIT /B
 
+@rem --- inscore compilation -------------------------
 :COMPILE
 	IF NOT EXIST  ..\modules\guidolib\build\lib\GUIDOEngine64.lib (
 		CALL :MODULES
@@ -52,8 +76,10 @@ EXIT /B
 echo Usage: %0 [COMMAND]
 echo where COMMAND is in:
 echo   modules : compile submodules
-echo   inscore : compile inscore
+echo   inscore : compile inscore (call modules when not already done)
 echo   deploy  : call windeployqt and copy dependent libraries
+echo   install : install inscore in current folder
+echo   package : build the inscore package
 echo   help    : display this help
 
 :DONE
