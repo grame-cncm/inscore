1ee7cc6ffc2f0b8b1ab6f0e2909f377e83999dbd
Dominique Fober <fober@grame.fr>
2021-02-15 10:13:29 +0100

    improve windows script

--- build/Make.bat
+++ build/Make.bat
@@ -5,14 +5,29 @@ set MSVC="Visual Studio 15 2017 Win64"
 set VERSION="1.2.7"
 
 IF [%1]==[]     GOTO USAGE
-IF %1==modules 	CALL :MODULES
-IF %1==inscore 	CALL :COMPILE
-IF %1==deploy 	CALL :DEPLOY
-IF %1==install 	CALL :INSTALL
-IF %1==package 	CALL :PACKAGE
+IF %1==modules (
+	CALL :MODULES
+	GOTO DONE
+)
+IF %1==inscore (
+	CALL :COMPILE
+	GOTO DONE
+)
+IF %1==deploy (
+	CALL :DEPLOY
+	GOTO DONE
+)
+IF %1==install (
+	CALL :INSTALL
+	GOTO DONE
+)
+IF %1==package (
+	CALL :PACKAGE
+	GOTO DONE
+)
 IF %1==help 	GOTO USAGE
 
-GOTO DONE
+GOTO USAGE
 
 @rem --- installation -------------------------------
 :INSTALL
@@ -81,5 +96,7 @@ echo   deploy  : call windeployqt and copy dependent libraries
 echo   install : install inscore in current folder
 echo   package : build the inscore package
 echo   help    : display this help
+echo This script is using %MSVC% to compile, edit 
+echo the script and set the MSVC variable to change. 
 
 :DONE
