5f61e88f43eca00c5bc58398bde6044682e2af07
Dominique Fober <fober@grame.fr>
2021-04-19 11:39:56 +0200

    remove commented code

--- javascript/src/JSImageView.ts
+++ javascript/src/JSImageView.ts
@@ -4,7 +4,6 @@
 class JSImageView extends JSAutoSize 
 {
     fImage: HTMLImageElement;
-    // fScale: number;
 
     constructor(parent: JSObjectView) {
 		let div = document.createElement('div');
@@ -16,7 +15,6 @@ class JSImageView extends JSAutoSize
         this.fImage = img;
         this.fImage.style.width = 100+"%";
         this.fImage.style.height = 100+"%";
-        // this.fScale = 1;
     }
 
     clone (parent: JSObjectView) : JSObjectView { 
@@ -25,39 +23,12 @@ class JSImageView extends JSAutoSize
         return img; 
     }
 
-// 	updateDimensions(pos: OPosition) : void {
-// console.log ("JSImageView.updateDimensions " + pos.width + " " + pos.height )
-//         this.fImage.width = this.relative2SceneWidth(pos.width);
-//         this.fImage.height = this.relative2SceneHeight(pos.height);
-//         super.updateDimensions (pos);
-//     }
-
-    // image scale is relative to the parent, by default fill the parent element (scale 1) 
-    // getScale(scale: number) : number {  
-	// 	let elt = this.getElement().parentElement;
-    //     let rw = this.fImage.clientWidth / elt.clientWidth;
-    //     let rh = this.fImage.clientHeight / elt.clientHeight;
-    //     this.fScale = scale / Math.max(rw, rh);
-    //     return this.fScale;
-    // }
-
 	toString() : string			{ return "JSImageView"; }
 	getSyncRatio()	: number    { return 1; }   // no scaling for images, appearance is already preserved 
-    getAutoSize() : Point       { return { x: this.fImage.naturalWidth, y: this.fImage.naturalHeight }; }
+    getAutoSize() : Point       { 
+        console.log ("JSImageView.getAutoSize " + this.fImage.naturalWidth + " " + this.fImage.naturalHeight)
+        return { x: this.fImage.naturalWidth, y: this.fImage.naturalHeight }; }
     getScale(scale: number) : number {  return scale; }
-
-// 	initView ( obj: INScoreObject)	: boolean {		
-//         this.fImage.src  = obj.getFile();
-//         this.fImage.onload = () => {
-// console.log("JSImageView.initView onload: " + this.fImage.width + ":" + this.fImage.height);
-//         }
-          
-// 		let elt = this.getElement();
-// 		let r = elt.getBoundingClientRect();
-
-// console.log ("JSImageView.initView: " + obj.getOSCAddress() + " " + this.fImage.width + ":" + this.fImage.height + " bb: " + r.width + ":" + r.height)
-// 		return true;
-// 	}
 
 	updateSpecial ( obj: INScoreObject)	: boolean {
         this.fImage.src  = obj.getFile();
