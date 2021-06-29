6b7ec0f5ce2562c7086f5f2ef4ea88a781d6342c
Dominique Fober <fober@grame.fr>
2021-06-29 08:20:13 +0200

    don't check for compiler (not required)

--- javascript/src/JSFaustwView.ts
+++ javascript/src/JSFaustwView.ts
@@ -15,24 +15,13 @@ class JSFaustwView extends JSFaustView {
     toString() : string		                    { return "JSFaustwView"; }
   
     async buildNodeFromWasm (obj: INScoreObject, wasm: string, json: string, voices: number) : Promise<number> {
-        if (JSFaustView.fCompilerLock) {
-            setTimeout ( () => { this.buildNodeFromWasm (obj, wasm, json, voices)}, 50);
-            return JSFaustView.kPending;
-        }
-        JSFaustView.fCompilerLock = true;
-        const name = "inscore";
         this.fFactory = await Faust.createGenerator().loadDSPFactory(wasm, json);
         if (!this.fFactory) return JSFaustView.kFailed;
-        let result = await this.makeAudioNode (obj, name, voices);
-        JSFaustView.fCompilerLock = false;
+        let result = await this.makeAudioNode (obj, "inscore", voices);
         return result;
     }
   
     updateSpecial(obj: INScoreObject)	: boolean {
-        if (!this.fFaust) {
-            console.log ("Faust engine is not available");
-            return false;
-        }
         AIOScanner.scan (obj.getOSCAddress());
         let data = obj.getFaustInfos (false, true);
         this.buildNodeFromWasm (obj, data.code, data.json, data.voices);
