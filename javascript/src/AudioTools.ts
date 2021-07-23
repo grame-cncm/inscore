3d98a3a3e148a13fe1ec6fa7b5ab1aeccb5dab45
Dominique Fober <fober@grame.fr>
2021-07-23 09:37:39 +0200

    audio io connections (in progress)

--- javascript/src/AudioTools.ts
+++ javascript/src/AudioTools.ts
@@ -3,20 +3,30 @@
 ///<reference path="JSObjectView.ts"/>
 ///<reference path="AudioObject.ts"/>
 
-interface cnxFunction { (src: AudioNode, dest: AudioNode) : boolean };
+interface cnxFunction { (src: AudioNode, dest: AudioNode, srcchan: number, destchan: number) : boolean };
 
 class AudioTools {
  
     static updateConnections(obj: INScoreObject, view: JSObjectView) {
         let cnx = obj.getAudioInfos();
+// console.log ("AudioTools: updateConnections connect: " + obj.getOSCAddress() + " " + cnx.connect.size() + " disconnect: " + cnx.disconnect.size())
         AudioTools.doit(view, cnx.connect, AudioTools.connectSrcDest, "connect");
         AudioTools.doit(view, cnx.disconnect, AudioTools.disconnectSrcDest, "disconnect");
     }
 
-    static connectSrcDest(src: AudioNode, dest: AudioNode) : boolean {
+    static connectSrcDest(src: AudioNode, dest: AudioNode, srcchan: number, destchan: number) : boolean {
         if (src && dest) {
             try {
-                src.connect(dest);
+                // if (srcchan) {
+console.log ("AudioTools:connectSrcDest src: " + src.numberOfInputs + " " + src.numberOfOutputs + ":" + src.channelCount  + " mode: " + src.channelCountMode + " - dest: " + dest.numberOfInputs + " " + dest.numberOfOutputs + ":" + dest.channelCount + " mode: " + dest.channelCountMode);
+console.log( "src.connect " + dest + "  " + srcchan + ", " + destchan);
+console.log(dest);
+console.log(src);
+                    src.connect(dest, srcchan, destchan);
+
+                // }
+                // else
+                //     src.connect(dest);
             }
             catch (error) {
                 console.log ("Exception while calling AudioNode connect: " + error);
@@ -26,10 +36,13 @@ class AudioTools {
         console.log ("AudioTools error: trying to connect null AudioNode (" + src + " " + dest + ")");
         return false;
     }
-    static disconnectSrcDest(src: AudioNode, dest: AudioNode) : boolean {
+    static disconnectSrcDest(src: AudioNode, dest: AudioNode, srcchan: number, destchan: number) : boolean {
         if (src && dest) {
             try {
-                src.disconnect(dest);
+                // if (srcchan)
+                    src.disconnect(dest, srcchan, destchan);
+                // else
+                //     src.disconnect(dest);
             }
             catch (error) {
                 console.log ("Exception while calling AudioNode disconnect: " + error);
@@ -40,21 +53,23 @@ class AudioTools {
         return false;
     }
 
-    static doit (view: JSObjectView, list: IntVector, cnx: cnxFunction, op: string) {
+    static doit (view: JSObjectView, list: AudioCnxVector, cnx: cnxFunction, op: string) {
         let n = list.size();
         for (let i = 0; i < n; i++) {
-            let tmp = JSObjectView.getObjectView(list.get(i));
+            let cdesc = list.get(i);
+            let tmp = JSObjectView.getObjectView(cdesc.objid);
             if (tmp) {
                 let dest = tmp.toAudioObject();
                 let src = view.toAudioObject();
-                if (src && dest)
-                    cnx (src.getAudioNode(), dest.getAudioNode());
+                if (src && dest) {
+                    cnx (src.getAudioNode(), dest.getAudioNode(), cdesc.from, cdesc.to);
+                }
                 else {
                     console.log ("AudioTools " + op + " error: not an audio object: " + view + " -> " + tmp);
                 }
             }
             else {
-                console.log ("AudioTools error: incorrect view received by " + op + " method: " + list.get(i));
+                console.log ("AudioTools error: incorrect view received by " + op + " method: " + cdesc.objid);
             }
         }
     }
