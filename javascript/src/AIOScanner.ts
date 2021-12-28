9bd9ef12a953dc5011999828f02a3a653d34a59b
Dominique Fober <fober@grame.fr>
2021-12-28 10:25:20 +0100

    audio context management

--- javascript/src/AIOScanner.ts
+++ javascript/src/AIOScanner.ts
@@ -11,13 +11,15 @@ class AIOScanner {
 
     static init()
     {
-        if (!AIOScanner.fAudioContext) {
-            AIOScanner.fAudioContext = new (window.AudioContext || window.webkitAudioContext)();
-            document.onreadystatechange = function() {
-                if (document.readyState === 'interactive') {
-                    AIOScanner.unlockAudioContext(AIOScanner.fAudioContext); 
-                }
-            }
+       if (!AIOScanner.fAudioContext) {
+        // console.log ("AIOScanner.init");
+        AIOScanner.fAudioContext = new (window.AudioContext || window.webkitAudioContext)();
+            AIOScanner.unlockAudioContext(AIOScanner.fAudioContext); 
+            // document.onreadystatechange = function() {
+            //     // if (document.readyState === 'interactive') {
+            //         AIOScanner.unlockAudioContext(AIOScanner.fAudioContext); 
+            //     // }
+            // }
         }
     }
 
@@ -46,8 +48,10 @@ class AIOScanner {
     private static unlock ()       { 
         AIOScanner.fUnlockEvents.forEach(e => document.body.removeEventListener(e, AIOScanner.unlock)); 
         AIOScanner.fAudioContext.resume();
+        // console.log ("unlock", AIOScanner.fAudioContext);
     } 
     private static unlockAudioContext(audioCtx: AudioContext) {
+        // console.log ("unlockAudioContext", audioCtx);
         if (audioCtx.state !== "suspended") return;
         AIOScanner.fUnlockEvents.forEach(e => document.body.addEventListener(e, AIOScanner.unlock, false));
     }
